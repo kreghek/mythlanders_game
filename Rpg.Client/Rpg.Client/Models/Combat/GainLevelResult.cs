a8fde9b9322a404aaeb5e045c84d2784694e8242
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-09-10 08:59:44 +0000

    [Bot] Automated ReSharper CLT update

--- Rpg.Client/Rpg.Client/Models/Combat/GainLevelResult.cs
+++ Rpg.Client/Rpg.Client/Models/Combat/GainLevelResult.cs
@@ -4,11 +4,10 @@ namespace Rpg.Client.Models.Combat
 {
     internal sealed record GainLevelResult
     {
+        public bool IsLevelUp => StartXp + XpAmount >= XpToLevelup;
+        public int StartXp { get; init; }
         public Unit Unit { get; init; }
         public int XpAmount { get; init; }
-        public int StartXp { get; init; }
         public int XpToLevelup { get; init; }
-
-        public bool IsLevelUp => StartXp + XpAmount >= XpToLevelup;
     }
 }
