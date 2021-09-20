54e96070d3c364ba489408f0f92de7e21bd89317
Gregory <lobster287@gmail.com>
2021-09-21 01:59:45 +0700

    Added save a game progress.

--- Rpg.Client/Rpg.Client/Models/GlobeProvider.cs
+++ Rpg.Client/Rpg.Client/Models/GlobeProvider.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 using Rpg.Client.Core;
+using Rpg.Client.Models.Save;
 
 namespace Rpg.Client.Models
 {
     internal sealed class GlobeProvider
     {
+        private const string SAVE_JSON = "save.json";
+
         private readonly IDice _dice;
+
+        private readonly string _saveFilePath;
+
         private Globe? _globe;
 
         public GlobeProvider(IDice dice)
         {
             _dice = dice;
+
+            var binPath = AppContext.BaseDirectory;
+            _saveFilePath = Path.Combine(binPath, SAVE_JSON);
         }
 
         public Globe Globe
@@ -28,6 +40,18 @@ namespace Rpg.Client.Models
                 return _globe;
             }
             private set => _globe = value;
+        }
+
+        public bool CheckExistsSave()
+        {
+            if (File.Exists(_saveFilePath))
+            {
+                return true;
+            }
+
+            Debug.WriteLine($"Not found a save file by provided path: {_saveFilePath}");
+
+            return false;
         }
 
         public void GenerateNew()
@@ -52,31 +76,87 @@ namespace Rpg.Client.Models
             Globe = globe;
         }
 
-        public void LoadGlobe()
+        public bool LoadGlobe()
         {
-            var binPath = AppContext.BaseDirectory;
-            var saveFilePath = Path.Combine(binPath, "globe.json");
-
-            var json = File.ReadAllText(saveFilePath);
-
-            var globe = JsonSerializer.Deserialize<Globe>(json);
-
-            if (globe is null)
+            if (!CheckExistsSave())
             {
-                throw new InvalidOperationException("Error during globe loading.");
+                return false;
             }
 
-            globe.UpdateNodes(_dice);
+            var json = File.ReadAllText(_saveFilePath);
 
-            Globe = globe;
+            var lastSave = JsonSerializer.Deserialize<SaveDto>(json);
+
+            if (lastSave is null)
+            {
+                throw new InvalidOperationException("Error during loading the last save.");
+            }
+
+            Globe = new Globe
+            {
+                Player = new Player
+                {
+                    Pool = GetSavedGroup(lastSave.Player.Pool),
+                    Group = GetSavedGroup(lastSave.Player.Group)
+                }
+            };
+
+            Globe.UpdateNodes(_dice);
+
+            return true;
         }
 
         public void StoreGlobe()
         {
-            var serializedGlobe = JsonSerializer.Serialize(_globe);
-            var binPath = AppContext.BaseDirectory;
-            var saveFilePath = Path.Combine(binPath, "globe.json");
-            File.WriteAllText(saveFilePath, serializedGlobe);
+            var save = new SaveDto
+            {
+                Combats = new List<Core.Combat>(),
+                Player = new PlayerParty
+                {
+                    Group = GetGroupToSave(Globe.Player.Group.Units),
+                    Pool = GetGroupToSave(Globe.Player.Pool.Units)
+                }
+            };
+            var serializedSave = JsonSerializer.Serialize(save);
+            File.WriteAllText(_saveFilePath, serializedSave);
+        }
+
+        private static GroupUnits GetGroupToSave(IEnumerable<Unit> units)
+        {
+            var savedUnits = units.Select(
+                unit => new UnitDto
+                {
+                    SchemeName = unit.UnitScheme.Name,
+                    Hp = unit.Hp,
+                    SkillSids = unit.Skills.Select(x => x.Sid),
+                    Xp = unit.Xp,
+                    Level = unit.Level
+                });
+
+            var savedGroup = new GroupUnits
+            {
+                Units = savedUnits
+            };
+
+            return savedGroup;
+        }
+
+        private static Group GetSavedGroup(GroupUnits groupUnits)
+        {
+            var restoredUnits = groupUnits.Units.Select(
+                                              unit => UnitSchemeCatalog.PlayerUnits.TryGetValue(
+                                                  unit.SchemeName,
+                                                  out var unitScheme)
+                                                  ? new Unit(unitScheme, unit.Level)
+                                                  : null)
+                                          .Where(x => x != null)
+                                          .Cast<Unit>();
+            var restoredGroup = new Group
+            {
+                Units = restoredUnits
+            };
+
+            return restoredGroup;
         }
     }
 }
---
+++ Rpg.Client/Rpg.Client/Models/Save/SaveDto.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+using Rpg.Client.Core;
+
+namespace Rpg.Client.Models.Save
+{
+    internal record SaveDto
+    {
+        public IList<Core.Combat> Combats { get; init; }
+
+        public PlayerParty Player { get; init; }
+    }
+
+    internal record PlayerParty
+    {
+        public GroupUnits Group { get; init; }
+
+        public GroupUnits Pool { get; init; }
+    }
+
+    internal record GroupUnits
+    {
+        public IEnumerable<UnitDto> Units { get; init; }
+    }
+
+    internal record UnitDto
+    {
+        public string SchemeName { get; init; }
+        
+        public int Hp { get; init; }
+        
+        public int Xp { get; init; }
+        
+        public int Level { get; init; }
+
+        public IEnumerable<string> SkillSids { get; init; }
+    }
+}
