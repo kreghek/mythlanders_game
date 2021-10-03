9e9ec6650947d42c8c670081383140f92b38a5e3
Gregory <lobster287@gmail.com>
2021-10-03 23:43:24 +0700

    Fixed spawning clouds

--- Rpg.Client/Rpg.Client/Models/Biome/BiomeScreen.cs
+++ Rpg.Client/Rpg.Client/Models/Biome/BiomeScreen.cs
@@ -207,14 +207,12 @@ namespace Rpg.Client.Models.Biome
 
         private Cloud CreateCloud(int index)
         {
-            var startPosition1 = new Vector2(
+            var endPosition = new Vector2(
                 Game.GraphicsDevice.Viewport.Width * 1.5f / CLOUD_COUNT * index -
                 Game.GraphicsDevice.Viewport.Width / 2,
                 Game.GraphicsDevice.Viewport.Height);
-            var endPosition1 = new Vector2(startPosition1.X + Game.GraphicsDevice.Viewport.Width / 2, 0);
-
-            var startPosition = endPosition1;
-            var endPosition = startPosition1;
+            const float START_VIEWPORT_Y_POSITION = -100;
+            var startPosition = new Vector2(endPosition.X + Game.GraphicsDevice.Viewport.Width / 2, START_VIEWPORT_Y_POSITION);
 
             var textureIndex = _random.Next(0, CLOUD_TEXTURE_COUNT);
             var speed = _random.NextDouble() + MAX_CLOUD_SPEED;
@@ -223,6 +221,7 @@ namespace Rpg.Client.Models.Biome
                 startPosition,
                 endPosition,
                 speed);
+            
             return cloud;
         }
 
