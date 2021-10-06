d864f696f824f72604c62a74a10ad1a68b99c4c4
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-10-06 11:45:55 +0700

    Clear up project files

--- Rpg.Client/Rpg.Client/Core/UnitScheme.cs
+++ Rpg.Client/Rpg.Client/Core/UnitScheme.cs
@@ -24,46 +24,4 @@ namespace Rpg.Client.Core
 
         public IReadOnlyList<SkillSet> SkillSets { get; init; }
     }
-
-    internal enum UnitName
-    { 
-        Undefined,
-
-        /// <summary>
-        /// Used only in the events to describe situations.
-        /// </summary>
-        Environment,
-
-        Berimir,
-        Hawk,
-        Rada,
-        Maosin,
-        Taochan,
-        Kakhotep,
-        Hq,
-        Oldman,
-        GuardianWoman,
-
-        Aspid,
-        Liho,  // лихо
-        Kikimore, // кикимора
-        SwampWomanFish,  // болотная русалка
-        Drekava, // Дрекавак
-        GreyWolf,
-        Bear,
-        Volkolak,
-        Dead, // Умертивие
-        Wisp,
-        Korgorush,
-        Stryga,
-        Vampire, // Вурдалак
-        HornedFrog, // Рогатая жаба
-        Basilisk,
-        Taote,
-        Sphynx,
-        Hydra,
-        KosheyTheImmortal,
-        KosheyTheImmortal2,
-        KosheyTheImmortal3,
-    }
 }
