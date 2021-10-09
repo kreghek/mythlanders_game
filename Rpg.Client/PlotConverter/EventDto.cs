e4232f3b943d0ffe1b98145d42a2105cfd5d895f
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-10-10 04:25:23 +0700

    Build event node graph from excel

---
+++ Rpg.Client/PlotConverter/EventDto.cs
@@ -0,0 +1,17 @@
+﻿namespace PlotConverter
+{
+    internal sealed record EventDto
+    {
+        public string Name { get; set; }
+
+        public EventNodeDto BeforeCombatNode { get; set; }
+        public EventNodeDto AfterCombatNode { get; set; }
+    }
+
+    internal sealed record EventNodeDto
+    { 
+        public string Speaker { get; set; }
+        public string Text { get; set; }
+        public EventNodeDto NextNode { get; set; }
+    }
+}
--- Rpg.Client/PlotConverter/Program.cs
+++ Rpg.Client/PlotConverter/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 
 using ExcelDataReader;
 
@@ -12,10 +12,81 @@ namespace PlotConverter
     {
         static void Main(string[] args)
         {
+            var excelRows = ReadFromExcel("Ewar - Plot.xlsx");
+
+            var eventDtoList = ConventExcelRowsToObjectGraph(excelRows);
+
+            var serialized = JsonSerializer.Serialize(eventDtoList);
+        }
+
+        private static List<EventDto> ConventExcelRowsToObjectGraph(List<ExcelRow> excelRows)
+        {
+            var eventGrouped = excelRows.GroupBy(x => x.Event);
+
+            var eventDtoList = new List<EventDto>();
+            foreach (var excelEventGroup in eventGrouped)
+            {
+                var eventDto = new EventDto
+                {
+                    Name = excelEventGroup.Key
+                };
+                eventDtoList.Add(eventDto);
+
+                var excelBeforeEventNodes = excelEventGroup.Where(x => x.CombatPosition == -1).ToList();
+                var beforeNode = BuildEventNodes(excelBeforeEventNodes);
+                eventDto.BeforeCombatNode = beforeNode;
+
+                var excelAfterEventNodes = excelEventGroup.Where(x => x.CombatPosition == 1).ToList();
+                var afterNode = BuildEventNodes(excelAfterEventNodes);
+                eventDto.AfterCombatNode = afterNode;
+            }
+
+            return eventDtoList;
+        }
+
+        private static EventNodeDto BuildEventNodes(List<ExcelRow> excelEventNodes)
+        {
+            var openList = new List<ExcelRow>(excelEventNodes);
+
+            EventNodeDto startNode = null;
+            EventNodeDto currentNode = null;
+            while (openList.Any())
+            {
+                var excelRowMinIndex = openList.Min(x => x.Index);
+                var targetExcelRow = openList.Single(x=>x.Index == excelRowMinIndex);
+
+                openList.Remove(targetExcelRow);
+
+                var node = new EventNodeDto
+                {
+                    Speaker = targetExcelRow.Speaker,
+                    Text = targetExcelRow.Text
+                };
+
+                if (startNode is null)
+                {
+                    startNode = node;
+                }
+
+                if (currentNode is null)
+                {
+                    currentNode = node;
+                }
+                else
+                {
+                    currentNode.NextNode = node;
+                    currentNode = node;
+                }
+            }
+
+            return startNode;
+        }
+
+        private static List<ExcelRow> ReadFromExcel(string filePath)
+        {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-
-            var filePath = "Ewar - Plot.xlsx";
+            var excelRows = new List<ExcelRow>();
             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
             {
                 // Auto-detect format, supports:
@@ -30,7 +101,7 @@ namespace PlotConverter
                     var rows = plotTable.Rows;
 
                     var isFirst = false;
-                    var excelRows = new List<ExcelRow>();
+
                     foreach (DataRow row in rows)
                     {
                         if (!isFirst)
@@ -45,15 +116,15 @@ namespace PlotConverter
                             Speaker = row[1] as string,
                             Text = row[2] as string,
                             Index = (int)((double?)row[3]).GetValueOrDefault(),
-                            CombatPosition = (int)((double?)row[4]).GetValueOrDefault()
+                            CombatPosition = (string)row[4] == "до" ? -1 : 1
                         };
 
                         excelRows.Add(excelRow);
                     }
-
-                    
                 }
             }
+
+            return excelRows;
         }
     }
 }
