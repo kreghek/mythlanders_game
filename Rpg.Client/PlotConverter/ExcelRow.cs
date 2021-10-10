60640a87358c967a1891ffe2c51476ca767f0c0d
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-10-10 15:15:29 +0700

    Reorganize converter models

--- Rpg.Client/PlotConverter/ExcelRow.cs
+++ Rpg.Client/PlotConverter/ExcelRow.cs
@@ -1,11 +1,19 @@
 ﻿namespace PlotConverter
 {
-    internal sealed record ExcelRow
+    internal sealed record ExcelTextFragmentRow
     {
-        public string Event { get; init; }
+        public string EventSid { get; init; }
         public string Speaker { get; init; }
         public string Text { get; init; }
         public int Index { get; init; }
         public int CombatPosition { get; init; }
+    }
+
+    internal sealed record ExcelEventRow
+    {
+        public string Sid { get; init; }
+        public string Name { get; init; }
+        public string Location { get; init; }
+        public string Aftermaths { get; init; }
     }
 }
