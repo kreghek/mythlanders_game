994ed28180aed6d37d9a0311240a05cd73484463
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-10-16 21:23:09 +0700

    Introduce particle system to visualize heal

---
+++ Rpg.Client/Rpg.Client/Models/Combat/GameObjects/HealLightObject.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Rpg.Client.Engine;
+
+namespace Rpg.Client.Models.Combat.GameObjects
+{
+    internal class Particle
+    {
+        public Texture2D Texture { get; set; }        // The texture that will be drawn to represent the particle
+        public Vector2 Position { get; set; }        // The current position of the particle        
+        public Vector2 Velocity { get; set; }        // The speed of the particle at the current instance
+        public float Angle { get; set; }            // The current angle of rotation of the particle
+        public float AngularVelocity { get; set; }    // The speed that the angle is changing
+        public Color Color { get; set; }            // The color of the particle
+        public float Size { get; set; }                // The size of the particle
+        public int TTL { get; set; }                // The 'time to live' of the particle
+
+        public Particle(Texture2D texture, Vector2 position, Vector2 velocity,
+            float angle, float angularVelocity, Color color, float size, int ttl)
+        {
+            Texture = texture;
+            Position = position;
+            Velocity = velocity;
+            Angle = angle;
+            AngularVelocity = angularVelocity;
+            Color = color;
+            Size = size;
+            TTL = ttl;
+        }
+
+        public void Update()
+        {
+            TTL--;
+            Position += Velocity;
+            Angle += AngularVelocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, 0, Texture.Width, Texture.Height);
+            Vector2 origin = new Vector2(Texture.Width / 2, Texture.Height / 2);
+
+            spriteBatch.Draw(Texture, Position, sourceRectangle, Color,
+                Angle, origin, Size, SpriteEffects.None, 0f);
+        }
+
+
+    }
+
+    internal class ParticleSystem
+    {
+        private Random random;
+        public Vector2 EmitterLocation { get; set; }
+        private IList<Particle> particles;
+        private IList<Texture2D> textures;
+
+        public ParticleSystem(IList<Texture2D> textures, Vector2 location)
+        {
+            EmitterLocation = location;
+            this.textures = textures;
+            this.particles = new List<Particle>();
+            random = new Random();
+        }
+
+        public void Update()
+        {
+            int total = 10;
+
+            for (int i = 0; i < total; i++)
+            {
+                particles.Add(GenerateNewParticle());
+            }
+
+            for (int particle = 0; particle < particles.Count; particle++)
+            {
+                particles[particle].Update();
+                if (particles[particle].TTL <= 0)
+                {
+                    particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                                    1f * (float)(random.NextDouble() * 2 - 1),
+                                    1f * (float)(random.NextDouble() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(random.NextDouble() * 2 - 1);
+            Color color = new Color(
+                        (float)random.NextDouble(),
+                        (float)random.NextDouble(),
+                        (float)random.NextDouble());
+            float size = (float)random.NextDouble();
+            int ttl = 20 + random.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int index = 0; index < particles.Count; index++)
+            {
+                particles[index].Draw(spriteBatch);
+            }
+            spriteBatch.End();
+        }
+    }
+
+
+
+
+    internal sealed class HealLightObject : IInteractionDelivery
+    {
+        private const double DURATION_SECONDS = 1.0;
+        private const double FRAMERATE = 1f / 8f;
+
+        private const int FRAME_COUNT = 4;
+
+        private readonly AnimationBlocker? _blocker;
+        private readonly Sprite _graphics;
+        private double _counter;
+        private double _frameCounter;
+        private int _frameIndex;
+
+        public HealLightObject(Vector2 targetPosition, GameObjectContentStorage contentStorage,
+            AnimationBlocker? blocker)
+        {
+            _graphics = new Sprite(contentStorage.GetBulletGraphics())
+            {
+                Position = targetPosition - Vector2.UnitY * 64
+            };
+            _blocker = blocker;
+        }
+
+        public bool IsDestroyed { get; private set; }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (IsDestroyed)
+            {
+                return;
+            }
+
+            _graphics.Draw(spriteBatch);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (IsDestroyed)
+            {
+                return;
+            }
+
+            if (_counter < DURATION_SECONDS)
+            {
+                _counter += gameTime.ElapsedGameTime.TotalSeconds;
+                _frameCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (_frameCounter >= FRAMERATE)
+                {
+                    _frameCounter = 0;
+                    _frameIndex++;
+
+                    if (_frameIndex > FRAME_COUNT - 1)
+                    {
+                        _frameIndex = 0;
+                    }
+                }
+
+                _graphics.SourceRectangle = new Rectangle(0, 32 * _frameIndex, 64, 32);
+            }
+            else
+            {
+                if (!IsDestroyed)
+                {
+                    IsDestroyed = true;
+                    _blocker?.Release();
+                }
+            }
+        }
+    }
+}
