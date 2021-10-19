53b890a7e89df39f742736e89d8aeb05d2c9828b
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-10-19 03:58:18 +0000

    [Bot] Automated ReSharper CLT update

--- Rpg.Client/Rpg.Client/Core/Skills/PowerUpSkill.cs
+++ Rpg.Client/Rpg.Client/Core/Skills/PowerUpSkill.cs
@@ -7,12 +7,12 @@ namespace Rpg.Client.Core.Skills
     internal sealed class PowerUpSkill : SkillBase
     {
         public PowerUpSkill() : base(new SkillVisualization
-        { Type = SkillVisualizationStateType.Support })
+            { Type = SkillVisualizationStateType.Support })
         {
         }
 
         public PowerUpSkill(bool costRequired) : base(new SkillVisualization
-        { Type = SkillVisualizationStateType.Support }, costRequired)
+            { Type = SkillVisualizationStateType.Support }, costRequired)
         {
         }
 
