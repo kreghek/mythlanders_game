baddbd0fc221794d0bbc99ff92649b761217b92d
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-02 22:54:58 +0700

    Fix storing location availability

--- Rpg.Client/Rpg.Client/Core/ProgressStorage/BiomeDto.cs
+++ Rpg.Client/Rpg.Client/Core/ProgressStorage/BiomeDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Rpg.Client.Core.ProgressStorage
+﻿using System.Collections.Generic;
+
+namespace Rpg.Client.Core.ProgressStorage
 {
     internal sealed record BiomeDto
     {
@@ -6,5 +8,12 @@
         public bool IsComplete { get; init; }
         public int Level { get; init; }
         public BiomeType Type { get; init; }
+        public IEnumerable<GlobeNodeDto?>? Nodes { get; init; }
+    }
+
+    internal sealed record GlobeNodeDto
+    { 
+        public GlobeNodeSid Sid { get; init; }
+        public bool IsAvailable { get; init; }
     }
 }
