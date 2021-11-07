6569a5c66494e809654e198dbca316e5a1d05faa
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-07 17:26:00 +0700

    Add specific volkolak skill with sound effect

--- Rpg.Client/Rpg.Client/Core/Skills/WideSlashSkill.cs
+++ Rpg.Client/Rpg.Client/Core/Skills/WideSlashSkill.cs
@@ -40,4 +40,55 @@ namespace Rpg.Client.Core.Skills
         public override SkillTargetType TargetType => SkillTargetType.Enemy;
         public override SkillType Type => SkillType.Melee;
     }
+
+    internal class VolkolakClawsSkill : SkillBase
+    {
+        public VolkolakClawsSkill() : this(false)
+        {
+        }
+
+        public VolkolakClawsSkill(bool costRequired) : base(new SkillVisualization
+        { Type = SkillVisualizationStateType.MassMelee, SoundEffectType = GameObjectSoundType.WolfBite },
+            costRequired)
+        {
+        }
+
+        public override IEnumerable<EffectRule> Rules { get; } = new List<EffectRule>
+        {
+            new EffectRule
+            {
+                Direction = SkillDirection.AllEnemy,
+                EffectCreator = new EffectCreator(u =>
+                {
+                    var res = new AttackEffect
+                    {
+                        PowerMultiplier = 0.5f,
+                        ValueRange = 1,
+                        Actor = u
+                    };
+
+                    return res;
+                })
+            },
+            new EffectRule
+            {
+                Direction = SkillDirection.Self,
+                EffectCreator = new EffectCreator(u =>
+                {
+                    var res = new HealEffect
+                    {
+                        Actor = u,
+                        PowerMultiplier = 0.5f,
+                        ValueRange = 1
+                    };
+
+                    return res;
+                })
+            }
+        };
+
+        public override string Sid => "Wide Slash";
+        public override SkillTargetType TargetType => SkillTargetType.Enemy;
+        public override SkillType Type => SkillType.Melee;
+    }
 }
