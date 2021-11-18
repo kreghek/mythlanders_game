418198240a18b51f5bf34a7b7d88230dbb1ae6b6
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-18 09:13:15 +0700

    Introduce the sprite of the Bear

--- Rpg.Client/Rpg.Client/Models/GameObjectContentStorage.cs
+++ Rpg.Client/Rpg.Client/Models/GameObjectContentStorage.cs
@@ -79,7 +79,8 @@ namespace Rpg.Client.Models
             _monsterUnitTextureDict = new Dictionary<UnitName, Texture2D>
             {
                 { UnitName.Aspid, contentManager.Load<Texture2D>("Sprites/GameObjects/MonsterUnits/Aspid") },
-                { UnitName.Wisp, contentManager.Load<Texture2D>("Sprites/GameObjects/MonsterUnits/Wisp") }
+                { UnitName.Wisp, contentManager.Load<Texture2D>("Sprites/GameObjects/MonsterUnits/Wisp") },
+                { UnitName.Bear, contentManager.Load<Texture2D>("Sprites/GameObjects/MonsterUnits/Bear") },
             };
 
             _combatBackgroundDict = new Dictionary<BackgroundType, Texture2D[]>
