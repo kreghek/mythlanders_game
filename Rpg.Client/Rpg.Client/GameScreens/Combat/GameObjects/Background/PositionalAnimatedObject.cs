8dd0652a34ab461a8b9082a44e845a1ab5be94b8
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-24 21:52:22 +0700

    Rename Rada + snow

--- Rpg.Client/Rpg.Client/GameScreens/Combat/GameObjects/Background/PositionalAnimatedObject.cs
+++ Rpg.Client/Rpg.Client/GameScreens/Combat/GameObjects/Background/PositionalAnimatedObject.cs
@@ -43,4 +43,25 @@ namespace Rpg.Client.GameScreens.Combat.GameObjects.Background
             }
         }
     }
+
+    internal sealed class WeatherAnimatedObject : IBackgroundObject
+    {
+        private ParticleSystem _particleSystem;
+
+        public WeatherAnimatedObject(Texture2D texture, Rectangle sourceRect)
+        {
+            var particleGenerator = new WeatherParticleGenerator(new[] { texture });
+            _particleSystem = new ParticleSystem(Vector2.Zero, particleGenerator);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _particleSystem.Draw(spriteBatch);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _particleSystem.Update(gameTime);
+        }
+    }
 }
