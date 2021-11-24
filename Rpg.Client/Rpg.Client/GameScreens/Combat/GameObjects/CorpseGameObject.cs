56a8d4d33b308f3db84f6b54f86f03868e36f19c
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-24 09:57:01 +0700

    WIP Corpses

---
+++ Rpg.Client/Rpg.Client/GameScreens/Combat/GameObjects/CorpseGameObject.cs
@@ -0,0 +1,103 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Rpg.Client.Engine;
+
+namespace Rpg.Client.GameScreens.Combat.GameObjects
+{
+    internal sealed class CorpseGameObject: EwarDrawableComponentBase
+    {
+        private bool _startToDeath;
+        private readonly UnitGraphics _graphics;
+        private readonly Camera2D _camera;
+        private readonly ScreenShaker _screenShaker;
+        private readonly GameObjectContentStorage _gameObjectContentStorage;
+
+        public CorpseGameObject(UnitGraphics graphics, Camera2D camera, ScreenShaker screenShaker, GameObjectContentStorage gameObjectContentStorage)
+        {
+            _graphics = graphics;
+            _camera = camera;
+            _screenShaker = screenShaker;
+            _gameObjectContentStorage = gameObjectContentStorage;
+        }
+
+        internal float GetZIndex()
+        {
+            return _graphics.Root.Position.Y;
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            
+            _graphics.Update(gameTime);
+
+            if (!_startToDeath)
+            {
+                _graphics.PlayAnimation("Death");
+                _startToDeath = true;
+            }
+        }
+
+        protected override void DoDraw(SpriteBatch spriteBatch, float zindex)
+        {
+            base.DoDraw(spriteBatch, zindex);
+            
+            _graphics.ShowActiveMarker = false;
+
+            if (_graphics.IsDamaged)
+            {
+                var allWhite = _gameObjectContentStorage.GetAllWhiteEffect();
+                spriteBatch.End();
+
+                var shakeVector = _screenShaker.GetOffset().GetValueOrDefault(Vector2.Zero);
+                var shakeVector3d = new Vector3(shakeVector, 0);
+
+                var worldTransformationMatrix = _camera.GetViewTransformationMatrix();
+                worldTransformationMatrix.Decompose(out var scaleVector, out _, out var translationVector);
+
+                var matrix = Matrix.CreateTranslation(translationVector + shakeVector3d)
+                             * Matrix.CreateScale(scaleVector);
+
+                spriteBatch.Begin(sortMode: SpriteSortMode.Deferred,
+                    blendState: BlendState.AlphaBlend,
+                    samplerState: SamplerState.PointClamp,
+                    depthStencilState: DepthStencilState.None,
+                    rasterizerState: RasterizerState.CullNone,
+                    transformMatrix: matrix,
+                    effect: allWhite);
+            }
+            else
+            {
+                spriteBatch.End();
+
+                var shakeVector = _screenShaker.GetOffset().GetValueOrDefault(Vector2.Zero);
+                var shakeVector3d = new Vector3(shakeVector, 0);
+
+                var worldTransformationMatrix = _camera.GetViewTransformationMatrix();
+                worldTransformationMatrix.Decompose(out var scaleVector, out _, out var translationVector);
+
+                var matrix = Matrix.CreateTranslation(translationVector + shakeVector3d)
+                             * Matrix.CreateScale(scaleVector);
+
+                spriteBatch.Begin(sortMode: SpriteSortMode.Deferred,
+                    blendState: BlendState.AlphaBlend,
+                    samplerState: SamplerState.PointClamp,
+                    depthStencilState: DepthStencilState.None,
+                    rasterizerState: RasterizerState.CullNone,
+                    transformMatrix: matrix);
+            }
+
+            _graphics.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            spriteBatch.Begin(sortMode: SpriteSortMode.Deferred,
+                blendState: BlendState.AlphaBlend,
+                samplerState: SamplerState.PointClamp,
+                depthStencilState: DepthStencilState.None,
+                rasterizerState: RasterizerState.CullNone,
+                transformMatrix: _camera.GetViewTransformationMatrix());
+        }
+    }
+}
