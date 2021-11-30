b4760e400c0c2ec3eb1d3ecc0f41883704e8a95a
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-11-30 23:19:38 +0700

    New wisp sprites

--- Rpg.Client/Rpg.Client/Core/GraphicConfigs/WispMonsterGraphicsConfig.cs
+++ Rpg.Client/Rpg.Client/Core/GraphicConfigs/WispMonsterGraphicsConfig.cs
@@ -8,14 +8,14 @@ namespace Rpg.Client.Core.GraphicConfigs
         {
             Animations = new Dictionary<AnimationSid, AnimationInfo>
             {
-                { AnimationSid.Idle, new AnimationInfo(startFrame: 0, frames: 8, speed: 8) },
+                { AnimationSid.Idle, new AnimationInfo(startFrame: 0, frames: 8, speed: 12) },
                 { AnimationSid.MoveForward, new AnimationInfo(startFrame: 0, frames: 1, speed: 1) },
                 { AnimationSid.MoveBackward, new AnimationInfo(startFrame: 0, frames: 1, speed: 1) },
-                { AnimationSid.Skill1, new AnimationInfo(startFrame: 8, frames: 4, speed: 4) },
+                { AnimationSid.Skill1, new AnimationInfo(startFrame: 8, frames: 8, speed: 12) },
                 { AnimationSid.Skill2, new AnimationInfo(startFrame: 0, frames: 1, speed: 1) },
                 { AnimationSid.Skill3, new AnimationInfo(startFrame: 0, frames: 1, speed: 1) },
-                { AnimationSid.Wound, new AnimationInfo(startFrame: 0, frames: 2, speed: 8) },
-                { AnimationSid.Death, new AnimationInfo(startFrame: 0, frames: 8, speed: 8) { IsFinal = true } }
+                { AnimationSid.Wound, new AnimationInfo(startFrame: 16, frames: 2, speed: 8) },
+                { AnimationSid.Death, new AnimationInfo(startFrame: 24, frames: 8, speed: 8) { IsFinal = true } }
             };
         }
     }
