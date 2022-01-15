9bec297bae10297e61e0c96b49d4b2fb28dcb18c
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-01-15 12:38:52 +0700

    WIP Introduce UpgradeScreen

---
+++ Rpg.Client/Rpg.Client/GameScreens/Upgrade/UpgradeScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Rpg.Client.Core;
+using Rpg.Client.ScreenManagement;
+
+namespace Rpg.Client.GameScreens.Upgrade
+{
+    internal sealed class UpgradeScreen : GameScreenBase
+    {
+        private readonly GlobeProvider _globeProvider;
+
+        public UpgradeScreen(EwarGame game) : base(game)
+        {
+            _globeProvider = game.Services.GetService<GlobeProvider>();
+        }
+
+        protected override void DrawContent(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateContent(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
