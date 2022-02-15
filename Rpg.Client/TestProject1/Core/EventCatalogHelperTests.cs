e99f383676b8a493b2575967cd7dfef3eeb56bec
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-02-15 23:48:02 +0700

    Improve tests

--- Rpg.Client/TestProject1/Core/EventCatalogHelperTests.cs
+++ Rpg.Client/TestProject1/Core/EventCatalogHelperTests.cs
@@ -102,5 +102,36 @@ namespace TestProject1.Core
 
             fact.Options.First().Next.Should().BeNull();
         }
+
+        [Test]
+        public void BuildEventNode_DoNotSplitIntoPages_AllTextFragmentsIntoSinglePage()
+        {
+            const int TEXT_FRAGMENT_COUNT = 10;
+            var nodeStorageModel = new EventNodeStorageModel
+            {
+                Fragments = Enumerable.Range(1, TEXT_FRAGMENT_COUNT).Select(x =>
+                    new EventTextFragmentStorageModel
+                    {
+                        Speaker = UnitName.Environment.ToString(),
+                        Text = $"test text test text test text test text {x}."
+                    }
+                ).ToArray()
+            };
+
+            var unitSchemeCatalogMock = new Mock<IUnitSchemeCatalog>();
+            var unitSchemeCatalog = unitSchemeCatalogMock.Object;
+
+            // ACT
+
+            var fact = EventCatalogHelper.BuildEventNode(nodeStorageModel,
+                EventPosition.BeforeCombat,
+                aftermath: null,
+                unitSchemeCatalog,
+                splitIntoPages: false);
+
+            // ASSERT
+
+            fact.TextBlock.Fragments.Should().HaveCount(TEXT_FRAGMENT_COUNT);
+        }
     }
 }
