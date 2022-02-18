eecaad4152bc6782707499cd6608774e3b188a90
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-02-18 16:08:39 +0700

    Read Berimir stats from balance table

--- Rpg.Client/Rpg.Client/Assets/Heroes/SwordsmanBuilder.cs
+++ Rpg.Client/Rpg.Client/Assets/Heroes/SwordsmanBuilder.cs
@@ -6,38 +6,35 @@ using Rpg.Client.Core.GraphicConfigs;
 
 namespace Rpg.Client.Assets.Heroes
 {
-    internal class SwordsmanBuilder : IHeroBuilder
+    internal class SwordsmanBuilder : HeroFactoryBase
     {
-        public UnitName UnitName { get; }
-
-        public UnitScheme Create(IBalanceTable balanceTable)
+        protected override IEquipmentScheme[] GetEquipment()
         {
-            return new()
+            return new IEquipmentScheme[]
             {
-                TankRank = 0.4f,
-                DamageDealerRank = 0.5f,
-                SupportRank = 0.1f,
-
-                Name = UnitName.Berimir,
-
-                Levels = new IUnitLevelScheme[]
-                {
-                    new AddSkillUnitLevel(1, new SwordSlashSkill()),
-                    new AddSkillUnitLevel(2, new WideSlashSkill()),
-                    new AddPerkUnitLevel(2, new ImprovedHitPoints()),
-                    new AddSkillUnitLevel(3, new DefenseStanceSkill(true)),
-                    new AddSkillUnitLevel(4, new SvarogBlastFurnaceSkill(true))
-                },
-
-                Equipments = new IEquipmentScheme[]
-                {
-                    new CombatSword(),
-                    new Mk2MediumPowerArmor(),
-                    new WoodenHandSculpture()
-                },
-
-                UnitGraphicsConfig = new BerimirGraphicsConfig()
+                new CombatSword(),
+                new Mk2MediumPowerArmor(),
+                new WoodenHandSculpture()
             };
         }
+
+        protected override IUnitLevelScheme[] GetLevels()
+        {
+            return new IUnitLevelScheme[]
+            {
+                new AddSkillUnitLevel(1, new SwordSlashSkill()),
+                new AddSkillUnitLevel(2, new WideSlashSkill()),
+                new AddPerkUnitLevel(2, new ImprovedHitPoints()),
+                new AddSkillUnitLevel(3, new DefenseStanceSkill(true)),
+                new AddSkillUnitLevel(4, new SvarogBlastFurnaceSkill(true))
+            };
+        }
+
+        protected override UnitGraphicsConfigBase GetGraphicsConfig()
+        {
+            return new BerimirGraphicsConfig();
+        }
+
+        public override UnitName UnitName => UnitName.Berimir;
     }
 }
