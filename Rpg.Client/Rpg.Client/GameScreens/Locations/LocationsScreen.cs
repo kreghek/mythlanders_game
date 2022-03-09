bc679f478b304bbcb7f7da507b952d043644731a
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-03-09 16:46:28 +0000

    [Bot] Automated dotnet-format update

--- Rpg.Client/Rpg.Client/GameScreens/Locations/LocationsScreen.cs
+++ Rpg.Client/Rpg.Client/GameScreens/Locations/LocationsScreen.cs
@@ -172,7 +172,7 @@ namespace Rpg.Client.GameScreens.Locations
                                 continue;
                             }
 
-                            var locationPanel = new LocationInfoPanel(node.Sid, _uiContentStorage.GetPanelTexture(), _uiContentStorage.GetButtonTexture(), _uiContentStorage.GetMainFont(), _uiContentStorage.GetMainFont(), ResolutionIndependentRenderer,biome, node, _globe, _unitSchemeCatalog, panelIndex);
+                            var locationPanel = new LocationInfoPanel(node.Sid, _uiContentStorage.GetPanelTexture(), _uiContentStorage.GetButtonTexture(), _uiContentStorage.GetMainFont(), _uiContentStorage.GetMainFont(), ResolutionIndependentRenderer, biome, node, _globe, _unitSchemeCatalog, panelIndex);
                             _locationInfoPanels.Add(locationPanel);
                             locationPanel.Selected += (_, _) =>
                             {
@@ -188,7 +188,7 @@ namespace Rpg.Client.GameScreens.Locations
                                     _resolutionIndependenceRenderer, _unitSchemeCatalog);
                                 AddModal(combatModal, isLate: false);
                             };
-                        }   
+                        }
                     }
 
                     _isNodeModelsCreated = true;
@@ -311,7 +311,7 @@ namespace Rpg.Client.GameScreens.Locations
             {
                 var col = panel.PanelIndex % 2;
                 var row = panel.PanelIndex / 2;
-                panel.Rect = new Rectangle(firstPanelX + col * PANEL_WIDTH, maxPanelClientRect.Top + row * PANEL_HEIGHT, PANEL_WIDTH, PANEL_HEIGHT);    
+                panel.Rect = new Rectangle(firstPanelX + col * PANEL_WIDTH, maxPanelClientRect.Top + row * PANEL_HEIGHT, PANEL_WIDTH, PANEL_HEIGHT);
                 panel.Draw(spriteBatch);
             }
         }
