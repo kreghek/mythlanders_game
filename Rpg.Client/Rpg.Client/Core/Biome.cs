18a23adc8887f8543256933904e932ca90045292
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-03-26 15:58:36 +0700

    Split biom and monste levels

--- Rpg.Client/Rpg.Client/Core/Biome.cs
+++ Rpg.Client/Rpg.Client/Core/Biome.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Rpg.Client.Core
 {
@@ -20,6 +21,8 @@ namespace Rpg.Client.Core
         public bool IsStart { get; init; }
 
         public int Level { get; set; }
+
+        public int MonsterLevel => (int)Math.Log(Level, 32) + 1;
 
         public int MinLevel { get; }
 
