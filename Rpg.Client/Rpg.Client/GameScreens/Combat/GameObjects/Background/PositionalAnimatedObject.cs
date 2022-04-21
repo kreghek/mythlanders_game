b5f718435a52720153eed7f22dadc582279279d5
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-04-21 09:44:56 +0700

    Add sprites of chinese floor

--- Rpg.Client/Rpg.Client/GameScreens/Combat/GameObjects/Background/PositionalAnimatedObject.cs
+++ Rpg.Client/Rpg.Client/GameScreens/Combat/GameObjects/Background/PositionalAnimatedObject.cs
@@ -43,31 +43,4 @@ namespace Rpg.Client.GameScreens.Combat.GameObjects.Background
             }
         }
     }
-
-    internal sealed class PositionalStaticObject : IBackgroundObject
-    {
-        private readonly Sprite _sprite;
-
-        public PositionalStaticObject(Texture2D texture, Vector2 position, Rectangle sourceRectangle, Vector2 origin)
-        {
-            _sprite = new Sprite(texture)
-            {
-                Position = position,
-                SourceRectangle = sourceRectangle,
-                Origin = origin
-            };
-        }
-
-        public Rectangle SourceRectangle { get; }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            _sprite.Draw(spriteBatch);
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            // Static objects does not updates.
-        }
-    }
 }
