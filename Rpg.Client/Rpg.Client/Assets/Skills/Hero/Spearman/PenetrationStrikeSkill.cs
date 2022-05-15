faf2074a86449f1c80a258c1e60fb513ee1c7512
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-05-15 19:38:05 +0700

    Refactor damage skills

--- Rpg.Client/Rpg.Client/Assets/Skills/Hero/Spearman/PenetrationStrikeSkill.cs
+++ Rpg.Client/Rpg.Client/Assets/Skills/Hero/Spearman/PenetrationStrikeSkill.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 
 using Rpg.Client.Core;
-using Rpg.Client.Core.SkillEffects;
 using Rpg.Client.Core.Skills;
 using Rpg.Client.GameScreens;
 using Rpg.Client.GameScreens.Combat;
@@ -22,22 +21,7 @@ namespace Rpg.Client.Assets.Skills.Hero.Spearman
 
         public override IReadOnlyList<EffectRule> Rules { get; } = new[]
         {
-            new EffectRule
-            {
-                Direction = SkillDirection.Target,
-                EffectCreator = new EffectCreator(u =>
-                {
-                    var equipmentMultiplier = u.Unit.GetEquipmentDamageMultiplierBonus(SID);
-
-                    var res = new DamageEffect
-                    {
-                        Actor = u,
-                        DamageMultiplier = 1 * equipmentMultiplier
-                    };
-
-                    return res;
-                })
-            }
+            SkillRuleFactory.CreateDamage(SID)
         };
 
         public override SkillSid Sid => SID;
