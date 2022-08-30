76207ba947dc1523d0120d8b1e266b8a05a4daef
Pavel Kurkutov <kurkutovpa@gmail.com>
2022-08-30 21:12:53 +0700

    Fix control textures

--- Rpg.Client/Rpg.Client/Engine/ControlTextures.cs
+++ Rpg.Client/Rpg.Client/Engine/ControlTextures.cs
@@ -4,6 +4,9 @@ namespace Rpg.Client.Engine
 {
     internal static class ControlTextures
     {
+        private const int COL_COUNT = 2;
+        private const int TEXTURE_SIZE = 32;
+
         public static Point Button { get; } = Point.Zero;
         public static Point Panel { get; } = CalcPointByIndex(1);
         public static Point Skill { get; } = CalcPointByIndex(2);
@@ -11,6 +14,6 @@ namespace Rpg.Client.Engine
         public static Point Option { get; } = CalcPointByIndex(4);
         public static Point Shadow { get; } = CalcPointByIndex(5);
 
-        private static Point CalcPointByIndex(int index) => new Point(index % 2, index / 2);
+        private static Point CalcPointByIndex(int index) => new Point(index % COL_COUNT * TEXTURE_SIZE, index / COL_COUNT * TEXTURE_SIZE);
     }
 }
