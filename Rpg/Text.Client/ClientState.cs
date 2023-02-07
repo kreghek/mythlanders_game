e78e464f33adec074fda4d24509f8279dd423d37
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-02-07 18:16:36 +0700

    States of client

--- Rpg/Text.Client/ClientState.cs
+++ Rpg/Text.Client/ClientState.cs
@@ -2,6 +2,7 @@ namespace Text.Client;
 
 public enum ClientState
 {
+    Initialize,
     Overview,
     CombatantInfo,
     MoveInfo
--- Rpg/Text.Client/Program.cs
+++ Rpg/Text.Client/Program.cs
@@ -5,7 +5,10 @@ using Stateless;
 
 using Text.Client;
 
-var clientStateMachine = new StateMachine<ClientState, ClientStateTrigger>(ClientState.Overview);
+var clientStateMachine = new StateMachine<ClientState, ClientStateTrigger>(ClientState.Initialize);
+
+clientStateMachine.Configure(ClientState.Initialize)
+    .Permit(ClientStateTrigger.OnOverview, ClientState.Overview);
 
 clientStateMachine.Configure(ClientState.Overview)
     .Permit(ClientStateTrigger.OnDisplayCombatantInfo, ClientState.CombatantInfo);
@@ -17,7 +20,6 @@ clientStateMachine.Configure(ClientState.CombatantInfo)
 clientStateMachine.Configure(ClientState.MoveInfo)
     .Permit(ClientStateTrigger.OnDisplayCombatantInfo, ClientState.CombatantInfo)
     .Permit(ClientStateTrigger.OnOverview, ClientState.Overview);
-    
 
 var combatCore = new CombatCore(new LinearDice());
 
@@ -46,25 +48,42 @@ combatCore.Initialize(
     new[]{new FormationSlot(1, 1){Combatant = monster}}
     );
 
+var roundIndex = 0;
+
 clientStateMachine.OnTransitionCompleted(transition =>
 {
     switch (transition.Destination)
     {
         case ClientState.Overview:
+            Console.WriteLine($"==== Round {roundIndex} ====");
+
+            HandleOverview(combatCore1: combatCore, stateMachine: clientStateMachine);
+
+            break;
+        
+        case ClientState.CombatantInfo:
+            var combatant = (transition.Parameters[0]) as Combatant;
             
+            HandleCombatantInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant);
+
+            break;
+        
+        case ClientState.MoveInfo:
+            var movement2 = (CombatMovement)transition.Parameters[0];
+            var combatant2 = (transition.Parameters[1]) as Combatant;
+            HandleMoveInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant2, movement2);
             break;
     }
 });
 
-var roundIndex = 0;
-while (true)
-{
-    Console.WriteLine($"==== Round {roundIndex} ====");
+clientStateMachine.Fire(ClientStateTrigger.OnOverview);
 
+void HandleOverview(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine)
+{
     Console.WriteLine("Round queue:");
-    for (var queueIndex = 0; queueIndex < combatCore.RoundQueue.Count; queueIndex++)
+    for (var queueIndex = 0; queueIndex < combatCore1.RoundQueue.Count; queueIndex++)
     {
-        var combatant = combatCore.RoundQueue[queueIndex];
+        var combatant = combatCore1.RoundQueue[queueIndex];
         Console.WriteLine($"{queueIndex}: {combatant.Sid}");
     }
 
@@ -72,39 +91,84 @@ while (true)
     {
         Console.WriteLine("Enter command:");
         var command = Console.ReadLine();
-
-        if (command == "list-moves")
+        
+        if (command == "info")
         {
-            var moveIndex = 0;
-            foreach (var movement in combatCore.Field.HeroSide[0, 1].Combatant.Hand)
-            {
-                if (movement is not null)
-                {
-                    Console.WriteLine($"{moveIndex}: {movement.Sid}");
-
-                    foreach (var effect in movement.Effects)
-                    {
-                        Console.WriteLine($"{effect}, {effect.Selector}, {effect.Lifetime}");
-                    }
-                }
-            }
+            var combatant = combatCore1.Field.HeroSide[0, 1].Combatant;
+            stateMachine.Fire(
+                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                    .OnDisplayCombatantInfo),
+                combatant);
+            break;
         }
+    }
+}
 
-        if (command == "move-info")
+void HandleCombatantInfo(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant)
+{
+    var moveIndex = 0;
+    foreach (var movement in targetCombatant.Hand)
+    {
+        if (movement is not null)
         {
-            var movement = combatCore.Field.HeroSide[0, 1].Combatant.Hand.First();
+            Console.WriteLine($"{moveIndex}: {movement.Sid}");
+        }
+    }
 
-            Console.WriteLine($"{movement.Sid}:");
-            foreach (var effect in movement.Effects)
-            {
-                Console.WriteLine($"{effect}, {effect.Selector}, {effect.Lifetime}");
-                var targets = effect.Selector.Get(combatCore.Field.HeroSide[0, 1].Combatant, combatCore.Field);
+    while (true)
+    {
+        Console.WriteLine("Enter command:");
+        var command = Console.ReadLine();
 
-                foreach (var combatant in targets)
-                {
-                    Console.WriteLine($"{combatant.Sid}");
-                }
-            }
+        if (command == "info")
+        {
+            var movement = targetCombatant.Hand.First();
+            
+            stateMachine.Fire(
+                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                    .OnDisplayMoveInfo),
+                movement,
+                targetCombatant);
+            break;
+        }
+        else if (command == "overview")
+        {
+            stateMachine.Fire(ClientStateTrigger.OnOverview);
+            break;
+        }
+    }
+}
+
+void HandleMoveInfo(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant1, CombatMovement movement1)
+{
+    Console.WriteLine($"{movement1.Sid} targets:");
+    foreach (var effect in movement1.Effects)
+    {
+        var targets = effect.Selector.Get(targetCombatant1, combatCore1.Field);
+
+        foreach (var combatant in targets)
+        {
+            Console.WriteLine($"> {combatant.Sid}");
+        }
+    }
+    
+    while (true)
+    {
+        Console.WriteLine("Enter command:");
+        var command = Console.ReadLine();
+
+        if (command == "back")
+        {
+            stateMachine.Fire(
+                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                    .OnDisplayCombatantInfo),
+                targetCombatant1);
+            break;
+        }
+        else if (command == "overview")
+        {
+            stateMachine.Fire(ClientStateTrigger.OnOverview);
+            break;
         }
     }
 }
