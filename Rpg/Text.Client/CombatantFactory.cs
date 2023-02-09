bd87f29bd986226c284e623c7f1563efe81b687f
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-02-09 12:02:31 +0700

    Refactor text client

--- Rpg/Text.Client/CombatantFactory.cs
+++ Rpg/Text.Client/CombatantFactory.cs
@@ -6,21 +6,30 @@ internal static class CombatantFactory
 {
     public static IReadOnlyCollection<FormationSlot> CreateHeroes()
     {
+        var swordsmanHero = CreateSwordsman();
+
+        return new[] { new FormationSlot(0, 1) { Combatant = swordsmanHero } };
+    }
+
+    private static Combatant CreateSwordsman()
+    {
         var heroSequence = new CombatMovementSequence();
         heroSequence.Items.Add(new CombatMovement("Die by sword!",
             new IEffect[]
             {
-                new DamageEffect(new ClosestInLineTargetSelector(), new InstantaneousEffectImposer(), Range<int>.CreateMono(2))
+                new DamageEffect(new ClosestInLineTargetSelector(), new InstantaneousEffectImposer(),
+                    Range<int>.CreateMono(2))
             })
         );
-        var hero = new Combatant(heroSequence) { Sid = "Berimir", IsPlayerControlled = true };
-
-        return new[] { new FormationSlot(0, 1) { Combatant = hero } };
+        var hero = new Combatant(heroSequence)
+        {
+            Sid = "Berimir", IsPlayerControlled = true
+        };
+        return hero;
     }
 
     public static IReadOnlyCollection<FormationSlot> CreateMonsters()
     {
-
         var monsterSequence = new CombatMovementSequence();
         monsterSequence.Items.Add(new CombatMovement("Cyber claws",
             new IEffect[]
--- Rpg/Text.Client/Program.cs
+++ Rpg/Text.Client/Program.cs
@@ -1,283 +1,300 @@
-﻿using Core.Combats;
+﻿using System.Text;
+
+using Core.Combats;
 using Core.Dices;
 
 using Stateless;
-using System.Text;
-using Text.Client;
 
-var clientStateMachine = StateMachineFactory.Create();
+namespace Text.Client;
 
-var combatCore = new CombatCore(new LinearDice());
-
-
-
-combatCore.Initialize(
-    CombatantFactory.CreateHeroes(),
-    CombatantFactory.CreateMonsters()
-    );
-
-var roundIndex = 0;
-
-clientStateMachine.OnTransitionCompleted(transition =>
+internal static class Program
 {
-    switch (transition.Destination)
+    public static void Main(string[] args)
     {
-        case ClientState.Overview:
-            Console.WriteLine($"==== Round {roundIndex} ====");
+        var clientStateMachine = StateMachineFactory.Create();
 
-            HandleOverview(combatCore1: combatCore, stateMachine: clientStateMachine);
+        var combatCore = new CombatCore(new LinearDice());
 
-            break;
-        
-        case ClientState.CombatantInfo:
-            var combatant = (transition.Parameters[0]) as Combatant;
-            
-            HandleCombatantInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant);
 
-            break;
-        
-        case ClientState.MoveInfo:
-            var movement2 = (CombatMovement)transition.Parameters[0];
-            var combatant2 = (transition.Parameters[1]) as Combatant;
-            HandleMoveInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant2, movement2);
-            break;
-    }
-});
 
-clientStateMachine.Fire(ClientStateTrigger.OnOverview);
+        combatCore.Initialize(
+            CombatantFactory.CreateHeroes(),
+            CombatantFactory.CreateMonsters()
+        );
 
-void HandleOverview(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine)
-{
-    while (true)
-    {
-        Console.WriteLine("Round queue:");
+        var roundIndex = 0;
 
-        // Print current queue
-        var queueSb = new StringBuilder();
-        for (var queueIndex = 0; queueIndex < combatCore1.RoundQueue.Count; queueIndex++)
+        clientStateMachine.OnTransitionCompleted(transition =>
         {
-            var combatant = combatCore1.RoundQueue[queueIndex];
-            queueSb.Append($" ({combatant.Sid.First()}) {combatant.Sid} |");
-        }
-        Console.WriteLine(queueSb.ToString());
-        Console.WriteLine(new string('=', 10));
-
-        // Print current field
-
-        for (var lineIndex = 0; lineIndex < 3; lineIndex++)
-        {
-            for (var columnIndex = 1; columnIndex >= 0; columnIndex--)
+            switch (transition.Destination)
             {
-                var coords = new FieldCoords(columnIndex, lineIndex);
-                var heroSlot = combatCore1.Field.HeroSide[coords];
-                if (heroSlot.Combatant is not null)
+                case ClientState.Overview:
+                    Console.WriteLine($"==== Round {roundIndex} ====");
+
+                    HandleOverview(combatCore1: combatCore, stateMachine: clientStateMachine);
+
+                    break;
+
+                case ClientState.CombatantInfo:
+                    var combatant = (transition.Parameters[0]) as Combatant;
+
+                    HandleCombatantInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant);
+
+                    break;
+
+                case ClientState.MoveInfo:
+                    var movement2 = (CombatMovement)transition.Parameters[0];
+                    var combatant2 = (transition.Parameters[1]) as Combatant;
+                    HandleMoveInfo(combatCore1: combatCore, stateMachine: clientStateMachine, combatant2, movement2);
+                    break;
+            }
+        });
+
+        clientStateMachine.Fire(ClientStateTrigger.OnOverview);
+
+
+    }
+
+    private static void HandleOverview(CombatCore combatCore1,
+        StateMachine<ClientState, ClientStateTrigger> stateMachine)
+    {
+        while (true)
+        {
+            Console.WriteLine("Round queue:");
+
+            // Print current queue
+            var queueSb = new StringBuilder();
+            for (var queueIndex = 0; queueIndex < combatCore1.RoundQueue.Count; queueIndex++)
+            {
+                var combatant = combatCore1.RoundQueue[queueIndex];
+                queueSb.Append($" ({combatant.Sid.First()}) {combatant.Sid} |");
+            }
+
+            Console.WriteLine(queueSb.ToString());
+            Console.WriteLine(new string('=', 10));
+
+            // Print current field
+
+            for (var lineIndex = 0; lineIndex < 3; lineIndex++)
+            {
+                for (var columnIndex = 1; columnIndex >= 0; columnIndex--)
                 {
-                    Console.Write($" ({heroSlot.Combatant.Sid.First()}) ");
+                    var coords = new FieldCoords(columnIndex, lineIndex);
+                    var heroSlot = combatCore1.Field.HeroSide[coords];
+                    if (heroSlot.Combatant is not null)
+                    {
+                        Console.Write($" ({heroSlot.Combatant.Sid.First()}) ");
+                    }
+                    else
+                    {
+                        Console.Write(" - - ");
+                    }
+                }
+
+                Console.Write("    ");
+
+                for (var columnIndex = 0; columnIndex < 2; columnIndex++)
+                {
+                    var coords = new FieldCoords(columnIndex, lineIndex);
+                    var monsterSlot = combatCore1.Field.MonsterSide[coords];
+                    if (monsterSlot.Combatant is not null)
+                    {
+                        Console.Write($" ({monsterSlot.Combatant.Sid.First()}) ");
+                    }
+                    else
+                    {
+                        Console.Write(" - - ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            // Print current combatant
+
+            Console.WriteLine($"Turn of {combatCore1.CurrentCombatant.Sid}");
+
+            // Print current combatant moves
+
+            Console.WriteLine("Moves:");
+            var mIndex = 0;
+            foreach (var movement3 in combatCore1.CurrentCombatant.Hand)
+            {
+                if (movement3 is not null)
+                {
+                    Console.WriteLine($"{mIndex}: {movement3.Sid}");
                 }
                 else
                 {
-                    Console.Write(" - - ");
+                    Console.WriteLine($"{mIndex}: -");
                 }
+
+                mIndex++;
             }
 
-            Console.Write("    ");
+            Console.WriteLine(new string('=', 10));
+            Console.WriteLine("- info {sid} - to display detailed combatant's info");
+            Console.WriteLine("- move {movement-index} - to use combat movement");
+            Console.WriteLine(new string('=', 10));
+            Console.WriteLine("Enter command:");
+            var command = Console.ReadLine();
 
-            for (var columnIndex = 0; columnIndex < 2; columnIndex++)
+            if (command.StartsWith("info"))
             {
-                var coords = new FieldCoords(columnIndex, lineIndex);
-                var monsterSlot = combatCore1.Field.MonsterSide[coords];
-                if (monsterSlot.Combatant is not null)
+                var combatant = GetCombatantByShortSid(combatCore1, command.Split(" ")[1]);
+
+                stateMachine.Fire(
+                    new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                        .OnDisplayCombatantInfo),
+                    combatant);
+                break;
+            }
+            else if (command.StartsWith("move"))
+            {
+                var split = command.Split(" ");
+
+                var moveNumber = int.Parse(split[1]);
+
+                var selectedMove = combatCore1.CurrentCombatant.Hand.Skip(moveNumber).Take(1).Single();
+
+                var movementExecution = combatCore1.UseCombatMovement(selectedMove);
+                PseudoPlayback(movementExecution);
+                combatCore1.CompleteTurn();
+            }
+        }
+    }
+
+    private static void HandleCombatantInfo(CombatCore combatCore1,
+        StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant)
+    {
+        Console.WriteLine("Stats:");
+        foreach (var stat in targetCombatant.Stats)
+        {
+            Console.WriteLine($"{stat.Type}: {stat.Value.Current}/{stat.Value.ActualMax}");
+        }
+
+        Console.WriteLine("Available movements:");
+        var moveIndex = 0;
+        foreach (var movement in targetCombatant.Hand)
+        {
+            if (movement is not null)
+            {
+                Console.WriteLine($"{moveIndex}: {movement.Sid}");
+            }
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Enter command:");
+            var command = Console.ReadLine();
+
+            if (command == "info")
+            {
+                var movement = targetCombatant.Hand.First();
+
+                stateMachine.Fire(
+                    new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                        .OnDisplayMoveInfo),
+                    movement,
+                    targetCombatant);
+                break;
+            }
+            else if (command == "overview")
+            {
+                stateMachine.Fire(ClientStateTrigger.OnOverview);
+                break;
+            }
+        }
+    }
+
+    private static void HandleMoveInfo(CombatCore combatCore1,
+        StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant1,
+        CombatMovement movement1)
+    {
+        var selectorContext = combatCore1.GetCurrentSelectorContext();
+        Console.WriteLine($"{movement1.Sid} targets:");
+        foreach (var effect in movement1.Effects)
+        {
+            var targets = effect.Selector.Get(targetCombatant1, selectorContext);
+
+            foreach (var combatant in targets)
+            {
+                Console.WriteLine($"> {combatant.Sid}");
+            }
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Enter command:");
+            var command = Console.ReadLine();
+
+            if (command == "back")
+            {
+                stateMachine.Fire(
+                    new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
+                        .OnDisplayCombatantInfo),
+                    targetCombatant1);
+                break;
+            }
+            else if (command == "overview")
+            {
+                stateMachine.Fire(ClientStateTrigger.OnOverview);
+                break;
+            }
+        }
+    }
+
+    static void PseudoPlayback(CombatMovementExecution movementExecution)
+    {
+        foreach (var imposeItem in movementExecution.EffectImposeItems)
+        {
+            foreach (var target in imposeItem.MaterializedTargets)
+            {
+                imposeItem.ImposeDelegate(target);
+            }
+        }
+    }
+
+    static Combatant? GetCombatantByShortSid(CombatCore combatCore1, string shortSid)
+    {
+        for (int colIndex = 0; colIndex < 2; colIndex++)
+        {
+            for (int lineIndex = 0; lineIndex < 3; lineIndex++)
+            {
+                Combatant? foundCombatant = null;
+                foundCombatant = CheckSlot(shortSid, colIndex, lineIndex, combatCore1.Field.HeroSide);
+
+                if (foundCombatant is not null)
                 {
-                    Console.Write($" ({monsterSlot.Combatant.Sid.First()}) ");
+                    return foundCombatant;
                 }
-                else
+
+                foundCombatant = CheckSlot(shortSid, colIndex, lineIndex, combatCore1.Field.MonsterSide);
+                if (foundCombatant is not null)
                 {
-                    Console.Write(" - - ");
+                    return foundCombatant;
                 }
             }
-
-            Console.WriteLine();
         }
 
-        // Print current combatant
+        return null;
+    }
 
-        Console.WriteLine($"Turn of {combatCore1.CurrentCombatant.Sid}");
-
-        // Print current combatant moves
-
-        Console.WriteLine("Moves:");
-        var mIndex = 0;
-        foreach (var movement3 in combatCore1.CurrentCombatant.Hand)
+    private static Combatant? CheckSlot(string shortSid, int colIndex, int lineIndex,
+        CombatFieldSide fieldSide)
+    {
+        var coords = new FieldCoords(colIndex, lineIndex);
+        var testCombatant = fieldSide[coords].Combatant;
+        if (testCombatant is not null)
         {
-            if (movement3 is not null)
+            var testShortSid = testCombatant.Sid.First();
+
+            if (testShortSid.ToString() == shortSid)
             {
-                Console.WriteLine($"{mIndex}: {movement3.Sid}");
-            }
-            else
-            {
-                Console.WriteLine($"{mIndex}: -");
-            }
-
-            mIndex++;
-        }
-
-        Console.WriteLine(new string('=', 10));
-        Console.WriteLine("- info {sid} - to display detailed combatant's info");
-        Console.WriteLine("- move {movement-index} - to use combat movement");
-        Console.WriteLine(new string('=', 10));
-        Console.WriteLine("Enter command:");
-        var command = Console.ReadLine();
-
-        if (command.StartsWith("info"))
-        {
-            var combatant = GetCombatantByShortSid(combatCore1, command.Split(" ")[1]);
-
-            stateMachine.Fire(
-                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
-                    .OnDisplayCombatantInfo),
-                combatant);
-            break;
-        }
-        else if (command.StartsWith("move"))
-        {
-            var split = command.Split(" ");
-
-            var moveNumber = int.Parse(split[1]);
-
-            var selectedMove = combatCore1.CurrentCombatant.Hand.Skip(moveNumber).Take(1).Single();
-
-            var movementExecution = combatCore1.UseCombatMovement(selectedMove);
-            PseudoPlayback(movementExecution);
-            combatCore1.CompleteTurn();
-        }
-    }
-}
-
-void HandleCombatantInfo(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant)
-{
-    Console.WriteLine("Stats:");
-    foreach (var stat in targetCombatant.Stats)
-    {
-        Console.WriteLine($"{stat.Type}: {stat.Value.Current}/{stat.Value.ActualMax}");
-    }
-
-    Console.WriteLine("Available movements:");
-    var moveIndex = 0;
-    foreach (var movement in targetCombatant.Hand)
-    {
-        if (movement is not null)
-        {
-            Console.WriteLine($"{moveIndex}: {movement.Sid}");
-        }
-    }
-
-    while (true)
-    {
-        Console.WriteLine("Enter command:");
-        var command = Console.ReadLine();
-
-        if (command == "info")
-        {
-            var movement = targetCombatant.Hand.First();
-            
-            stateMachine.Fire(
-                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
-                    .OnDisplayMoveInfo),
-                movement,
-                targetCombatant);
-            break;
-        }
-        else if (command == "overview")
-        {
-            stateMachine.Fire(ClientStateTrigger.OnOverview);
-            break;
-        }
-    }
-}
-
-void HandleMoveInfo(CombatCore combatCore1, StateMachine<ClientState, ClientStateTrigger> stateMachine, Combatant targetCombatant1, CombatMovement movement1)
-{
-    var selectorContext = combatCore1.GetCurrentSelectorContext();
-    Console.WriteLine($"{movement1.Sid} targets:");
-    foreach (var effect in movement1.Effects)
-    {
-        var targets = effect.Selector.Get(targetCombatant1, selectorContext);
-
-        foreach (var combatant in targets)
-        {
-            Console.WriteLine($"> {combatant.Sid}");
-        }
-    }
-
-    while (true)
-    {
-        Console.WriteLine("Enter command:");
-        var command = Console.ReadLine();
-
-        if (command == "back")
-        {
-            stateMachine.Fire(
-                new StateMachine<ClientState, ClientStateTrigger>.TriggerWithParameters(ClientStateTrigger
-                    .OnDisplayCombatantInfo),
-                targetCombatant1);
-            break;
-        }
-        else if (command == "overview")
-        {
-            stateMachine.Fire(ClientStateTrigger.OnOverview);
-            break;
-        }
-    }
-}
-
-static void PseudoPlayback(CombatMovementExecution movementExecution)
-{
-    foreach (var imposeItem in movementExecution.EffectImposeItems)
-    {
-        foreach (var target in imposeItem.MaterializedTargets)
-        {
-            imposeItem.ImposeDelegate(target);
-        }
-    }
-}
-
-static Combatant? GetCombatantByShortSid(CombatCore combatCore1, string shortSid)
-{
-    for (int colIndex = 0; colIndex < 2; colIndex++)
-    {
-        for (int lineIndex = 0; lineIndex < 3; lineIndex++)
-        {
-            Combatant? foundCombatant = null;
-            foundCombatant = CheckSlot(combatCore1, shortSid, colIndex, lineIndex, combatCore1.Field.HeroSide);
-
-            if (foundCombatant is not null)
-            {
-                return foundCombatant;
-            }
-
-            foundCombatant = CheckSlot(combatCore1, shortSid, colIndex, lineIndex, combatCore1.Field.MonsterSide);
-            if (foundCombatant is not null)
-            {
-                return foundCombatant;
+                return testCombatant;
             }
         }
+
+        return null;
     }
-    return null;
-}
-
-static Combatant? CheckSlot(CombatCore combatCore1, string shortSid, int colIndex, int lineIndex, CombatFieldSide fieldSide)
-{
-    var coorods = new FieldCoords(colIndex, lineIndex);
-    var testCombatant = fieldSide[coorods].Combatant;
-    if (testCombatant is not null)
-    {
-        var testShortSid = testCombatant.Sid.First();
-
-        if (testShortSid.ToString() == shortSid)
-        {
-            return testCombatant;
-        }
-    }
-
-    return null;
 }
