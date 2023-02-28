b2c1c7278929fd41e5825d5047f288ee44132d89
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-02-28 20:55:14 +0700

    Fix synth's quest stage 2 and 3

--- Rpg/Client/Assets/Catalogs/CampaignGenerator.cs
+++ Rpg/Client/Assets/Catalogs/CampaignGenerator.cs
@@ -14,14 +14,18 @@ namespace Client.Assets.Catalogs;
 
 internal sealed class CampaignGenerator : ICampaignGenerator
 {
+    private readonly GlobeProvider _globeProvider;
     private readonly IDice _dice;
+    private readonly IJobProgressResolver _jobProgressResolver;
     private readonly CampaignStageTemplateServices _services;
 
     public CampaignGenerator(IUnitSchemeCatalog unitSchemeCatalog, GlobeProvider globeProvider,
-        IEventCatalog eventCatalog, IDice dice)
+        IEventCatalog eventCatalog, IDice dice, IJobProgressResolver jobProgressResolver)
     {
         _services = new CampaignStageTemplateServices(unitSchemeCatalog, eventCatalog, globeProvider, dice);
+        _globeProvider = globeProvider;
         _dice = dice;
+        _jobProgressResolver = jobProgressResolver;
     }
 
     private HeroCampaign CreateCampaign(LocationSid locationSid)
@@ -48,7 +52,7 @@ internal sealed class CampaignGenerator : ICampaignGenerator
             stages.Add(stage);
         }
 
-        var rewardStageItem = new RewardStageItem(this);
+        var rewardStageItem = new RewardStageItem(this, _globeProvider, _jobProgressResolver);
         var rewardStage = new CampaignStage
         {
             Items = new[]
