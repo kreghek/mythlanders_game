1f889ffe15c006a2e25aaef717beef834c19758b
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-06 16:23:05 +0700

    Fixing side quest quests

--- Rpg.Client/Content.SideQuests.Tests/LocalDialogueResourceProvider.cs
+++ Rpg.Client/Content.SideQuests.Tests/LocalDialogueResourceProvider.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 using Rpg.Client.Assets.Catalogs;
 
 namespace Content.SideQuests.Tests;
@@ -6,6 +8,14 @@ public class LocalDialogueResourceProvider: IDialogueResourceProvider
 {
     public string GetResource(string resourceSid)
     {
-        return File.ReadAllText(Path.Combine("../Client/Content/Dialogues", resourceSid + ".yaml"));
+        int n = 1;
+        string[] lines = File.ReadAllText(Path.Combine("Content","Dialogues", resourceSid + ".xnb"), Encoding.UTF8)
+            .Split(Environment.NewLine.ToCharArray())
+            .Skip(n)
+            .ToArray();
+
+        string output = string.Join(Environment.NewLine, lines);
+        
+        return output;
     }
 }
--- Rpg/Client/Assets/Catalogs/DialogueStoring/DialogueDtoData.cs
+++ Rpg/Client/Assets/Catalogs/DialogueStoring/DialogueDtoData.cs
@@ -1,6 +1,6 @@
 namespace Client.Assets.Catalogs.DialogueStoring;
 
-abstract class DialogueDtoData
+internal class DialogueDtoData
 {
     public string Type{get;set;}
     public string Data{get;set;}
