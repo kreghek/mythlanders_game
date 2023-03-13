c0449ae77806857c81c9e9c3b8da8b259c2fbcd7
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-13 23:45:02 +0700

    Fix build

--- Rpg/Text.Client/BotCombatActorBehaviour.cs
+++ Rpg/Text.Client/BotCombatActorBehaviour.cs
@@ -4,17 +4,12 @@ namespace Text.Client;
 
 internal sealed class BotCombatActorBehaviour : ICombatActorBehaviour
 {
-    public void HandleIntention(ICombatActorBehaviourData combatData, Action<IIntention> intentionDelegate)
+    void ICombatActorBehaviour.HandleIntention(ICombatActorBehaviourData combatData, Action<IIntention> intentionDelegate)
     {
         var firstSkill = combatData.CurrentActor.Skills.First();
 
         var skillIntention = new UseCombatMovementIntention(firstSkill.CombatMovement);
 
         intentionDelegate(skillIntention);
-    }
-
-    public void HandleIntention(ICombatActorBehaviourData combatData, Action<IIntention> intentionDelegate)
-    {
-        throw new System.NotImplementedException();
     }
 }
