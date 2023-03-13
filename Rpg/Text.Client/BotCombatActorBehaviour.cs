d93fa7fe80b6f7ce94cb8fa147f055331b144170
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-13 23:50:25 +0700

    Upgrade dotnet-format

--- Rpg/Text.Client/BotCombatActorBehaviour.cs
+++ Rpg/Text.Client/BotCombatActorBehaviour.cs
@@ -2,14 +2,13 @@
 
 namespace Text.Client;
 
+/// <summary>
+/// Bot behaviour for text client.
+/// </summary>
 internal sealed class BotCombatActorBehaviour : ICombatActorBehaviour
 {
+    /// <inheritdoc/>
     public void HandleIntention(ICombatActorBehaviourData combatData, Action<IIntention> intentionDelegate)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    void ICombatActorBehaviour.HandleIntention(ICombatActorBehaviourData combatData, Action<IIntention> intentionDelegate)
     {
         var firstSkill = combatData.CurrentActor.Skills.First();
 
