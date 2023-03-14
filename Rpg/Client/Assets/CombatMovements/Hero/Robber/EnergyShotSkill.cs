2b5ba0957d68a60d344d7000f5573bba4e514bd7
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-14 23:44:26 +0700

    Mute current skills - 2

---
+++ Rpg/Client/Assets/CombatMovements/Hero/Robber/EnergyShotSkill.cs
@@ -0,0 +1,36 @@
+﻿//using System.Collections.Generic;
+
+//using JetBrains.Annotations;
+
+//using Rpg.Client.Core;
+//using Rpg.Client.GameScreens;
+
+//namespace Rpg.Client.Assets.Skills.Hero.Archer
+//{
+//    [UsedImplicitly]
+//    internal class EnergyShotSkill : VisualizedSkillBase
+//    {
+//        private const SkillSid SID = SkillSid.EnergyShot;
+
+//        public EnergyShotSkill() : base(PredefinedVisualization, false)
+//        {
+//        }
+
+//        public override IReadOnlyList<EffectRule> Rules { get; } = new List<EffectRule>
+//        {
+//            SkillRuleFactory.CreateDamage(SID)
+//        };
+
+//        public override SkillSid Sid => SID;
+//        public override SkillTargetType TargetType => SkillTargetType.Enemy;
+//        public override SkillType Type => SkillType.Range;
+
+//        private static SkillVisualization PredefinedVisualization => new()
+//        {
+//            Type = SkillVisualizationStateType.Range,
+//            SoundEffectType = GameObjectSoundType.EnergoShot,
+//            IconOneBasedIndex = 5,
+//            AnimationSid = PredefinedAnimationSid.Skill1
+//        };
+//    }
+//}
---
+++ Rpg/Core/Combats/Effects/PeriodicEffect.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Combats.Effects;
+
+public sealed class PeriodicEffect : IEffect
+{
+    public PeriodicEffect(ITargetSelector selector, IEffect baseEffect, ICombatantEffectLifetime lifetime)
+    {
+        Selector = selector;
+        BaseEffect = baseEffect;
+        Lifetime = lifetime;
+    }
+
+    public ITargetSelector Selector { get; }
+    public IEffect BaseEffect { get; }
+    public ICombatantEffectLifetime Lifetime { get; }
+
+    public IReadOnlyCollection<IEffectCondition> ImposeConditions => Array.Empty<IEffectCondition>();
+
+    public IEffectInstance CreateInstance()
+    {
+        return new PeriodicEffectInstance(this);
+    }
+}
