3b3b547dfc26b6357ebc652c6784e7c207241aa2
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-23 23:32:36 +0700

    Chaser variations

--- Rpg/Client/GameScreens/Combat/CombatDebugElements/ThiefChaserFartory.cs
+++ Rpg/Client/GameScreens/Combat/CombatDebugElements/ThiefChaserFartory.cs
@@ -1,39 +1,14 @@
-using System.Collections.Generic;
-
 using Client.Assets.CombatMovements.Monster.Slavic.DigitalWolf;
 
 using Core.Combats;
-using Core.Combats.CombatantEffectLifetimes;
-using Core.Combats.Effects;
-using Core.Combats.TargetSelectors;
 
 namespace Client.GameScreens.Combat.CombatDebugElements;
 
 public class ThiefChaserFactory
 {
-    public Combatant Create(string sid, ICombatActorBehaviour combatActorBehaviour)
+    public Combatant Create(string sid, ICombatActorBehaviour combatActorBehaviour, int variationIndex)
     {
-        var list = new List<CombatMovement>
-        {
-            new ChainHitFactory().CreateMovement(),
-
-            new DoubleKapeshFactory().CreateMovement(),
-
-            new ChasingFactory().CreateMovement(),
-
-            new GuardianPromiseFactory().CreateMovement(),
-
-            new AfterlifeWhirlwindFactory().CreateMovement()
-        };
-
-        var monsterSequence = new CombatMovementSequence();
-        for (var i = 0; i < 2; i++)
-        {
-            foreach (var movement in list)
-            {
-                monsterSequence.Items.Add(movement);
-            }
-        }
+        var monsterSequence = CreateCombatMoveVariation(variationIndex);
 
         var stats = new CombatantStatsConfig();
         stats.SetValue(UnitStatType.HitPoints, 5);
@@ -46,5 +21,34 @@ public class ThiefChaserFactory
         };
 
         return monster;
+    }
+
+    private static CombatMovementSequence CreateCombatMoveVariation(int variationIndex)
+    {
+        var moveTemplate = new CombatMovement[,]
+        {
+            { new ChainHitFactory().CreateMovement(), new ChasingFactory().CreateMovement() },
+
+            { new DoubleKapeshFactory().CreateMovement(), new AfterlifeWhirlwindFactory().CreateMovement() },
+
+            { new ChasingFactory().CreateMovement(), new GuardianPromiseFactory().CreateMovement() },
+
+            { new GuardianPromiseFactory().CreateMovement(), new DoubleKapeshFactory().CreateMovement() },
+
+            { new AfterlifeWhirlwindFactory().CreateMovement(), new ChainHitFactory().CreateMovement() }
+        };
+
+        var monsterSequence = new CombatMovementSequence();
+
+        for (var i = 0; i < 2; i++)
+        {
+            for (var j = 0; j < moveTemplate.GetLength(0); j++)
+            {
+                var combatMovement = moveTemplate[j, variationIndex];
+                monsterSequence.Items.Add(combatMovement);
+            }
+        }
+
+        return monsterSequence;
     }
 }
