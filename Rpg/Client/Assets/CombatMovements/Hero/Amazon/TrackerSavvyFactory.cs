da5c976416627ab6a15a9dd8118d97572d06d27b
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-03-24 16:15:56 +0700

    Reduce cost on tracker savvy

--- Rpg/Client/Assets/CombatMovements/Hero/Amazon/TrackerSavvyFactory.cs
+++ Rpg/Client/Assets/CombatMovements/Hero/Amazon/TrackerSavvyFactory.cs
@@ -1,21 +1,33 @@
 ﻿using Core.Combats;
+using Core.Combats.CombatantEffects;
 using Core.Combats.Effects;
 using Core.Combats.TargetSelectors;
 
+using JetBrains.Annotations;
+
 namespace Client.Assets.CombatMovements.Hero.Amazon;
 
+[UsedImplicitly]
 internal class TrackerSavvyFactory : CombatMovementFactoryBase
 {
+    /// <inheritdoc />
     public override CombatMovementIcon CombatMovementIcon => new(1, 7);
 
+    /// <inheritdoc />
     public override CombatMovement CreateMovement()
     {
+        var combatantEffectFactory = new ModifyCombatantMoveStatsCombatantEffectFactory(
+            new MultipleCombatantTurnEffectLifetimeFactory(1),
+            CombatantMoveStats.Cost,
+            -1);
+        
         return new CombatMovement(Sid,
             new CombatMovementCost(1),
             CombatMovementEffectConfig.Create(
                 new IEffect[]
                 {
-                    new ModifyEffectsEffect(new SelfTargetSelector(), 1)
+                    new ModifyEffectsEffect(new SelfTargetSelector(), 1),
+                    new AddCombatantEffectEffect(new SelfTargetSelector(), combatantEffectFactory)
                 })
         );
     }
