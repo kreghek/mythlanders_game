b43ab028b6701bf8169fabd565814cddf398b143
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-02 11:22:28 +0700

    Docs

--- Rpg/Client/Assets/CombatSoundtrack.cs
+++ Rpg/Client/Assets/CombatSoundtrack.cs
@@ -4,6 +4,9 @@ using Rpg.Client.Engine;
 
 namespace Client.Assets;
 
+/// <summary>
+/// Metadata of combat soundtrack to select correct track in a combats.
+/// </summary>
 internal record CombatSoundtrack(LocationCulture Culture, Song Soundtrack, CombatSoundtrackRole SoundtrackRole)
 {
     public CombatSoundtrack(LocationCulture culture, Song soundtrack):this(culture, soundtrack, CombatSoundtrackRole.Regular)
