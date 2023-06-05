3139f88ff4faf08be6e1cce7e8ef86798e1d0b31
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-05 22:27:16 +0700

    Reorganize the project

--- Rpg.Client/LeDialoduesEditorResGenerator/Serialization/DialogueDtoOption.cs
+++
@@ -1,9 +0,0 @@
-namespace LeDialoduesEditorResGenerator.Serialization;
-
-internal class DialogueDtoOption
-{
-    public DialogueDtoData[] Aftermaths { get; set; }
-    public DialogueDtoData[] Conditions { get; set; }
-    public string Next { get; set; }
-    public string Text { get; set; }
-}
--- Rpg.Client/LeDialoduesEditorResGenerator/Serialization/DialogueDtoReaction.cs
+++
@@ -1,8 +0,0 @@
-namespace LeDialoduesEditorResGenerator.Serialization;
-
-internal class DialogueDtoReaction
-{
-    public string Hero { get; set; }
-    public DialogueDtoParagraph[] Paragraphs { get; set; }
-    public string Text { get; set; }
-}
