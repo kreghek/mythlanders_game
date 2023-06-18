2188c1bd3720eac5077bde72e7c34dbcc1dccff6
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-18 21:57:13 +0700

    Add automataur monster in the game

---
+++ src/Client/Assets/Monsters/Greek/AutomataurFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+using Client.Assets.CombatMovements;
+using Client.Assets.GraphicConfigs;
+using Client.Assets.GraphicConfigs.Monsters;
+using Client.Core;
+using Client.GameScreens;
+
+namespace Client.Assets.Monsters.Greek;
+internal class AutomataurFactory: MonsterFactoryBase
+{
+    public override UnitName ClassName => UnitName.Automataur;
+
+    public override CharacterCultureSid Culture => CharacterCultureSid.Greek;
+
+    public override UnitScheme Create(IBalanceTable balanceTable)
+    {
+        return new UnitScheme(balanceTable.GetCommonUnitBasics());
+    }
+
+    public override UnitGraphicsConfigBase CreateGraphicsConfig(GameObjectContentStorage gameObjectContentStorage)
+    {
+        return new SingleSpriteGraphicsConfig(Path.Combine(CommonConstants.PathToCharacterSprites, "Monsters", Culture.ToString(),
+            ClassName.ToString(), "Thumbnail"));
+    }
+}
