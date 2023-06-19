0141b4c7682a0b831bb6b19f49ed2abfc2d60bd2
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-19 22:23:02 +0700

    Fix build

--- src/Client/GameScreens/Combat/CombatDebugElements/AutomataurCombatantFactory.cs
+++ src/Client/GameScreens/Combat/CombatDebugElements/AutomataurCombatantFactory.cs
@@ -46,7 +46,7 @@ public class AutomataurCombatantFactory : IMonsterCombatantFactory
 
         var monster = new Combatant("automataur", monsterSequence, stats, combatActorBehaviour)
         {
-            Sid = sid, IsPlayerControlled = false
+            DebugSid = sid, IsPlayerControlled = false
         };
 
         return monster;
