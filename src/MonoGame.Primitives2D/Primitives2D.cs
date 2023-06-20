e5e437e5cce9879fe1b4fc30e62b36c582e171a3
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-20 19:01:35 +0700

    Configure projects

--- src/MonoGame.Primitives2D/Primitives2D.cs
+++ src/MonoGame.Primitives2D/Primitives2D.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 // ReSharper disable once CheckNamespace
