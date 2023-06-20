698972f98d79059175dfbafd3f6890e16aaaf27f
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-20 23:56:44 +0700

    Disable analysis

--- src/Core.Balance/BalanceTableRecord.cs
+++ src/Core.Balance/BalanceTableRecord.cs
@@ -1,9 +1,3 @@
 ﻿namespace Core.Balance;
 
-public sealed class BalanceTableRecord
-{
-    public float DamageDealerRank { get; init; }
-    public string Sid { get; init; }
-    public float SupportRank { get; init; }
-    public float TankRank { get; init; }
-}
+public sealed record BalanceTableRecord(float DamageDealerRank, string Sid, float SupportRank, float TankRank);
