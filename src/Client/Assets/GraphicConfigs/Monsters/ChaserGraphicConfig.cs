e1b5e45843a3608e854ff895b20cd9b1dd5a3028
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-07-24 21:05:04 +0700

    Improve shadow placement

--- src/Client/Assets/GraphicConfigs/Monsters/ChaserGraphicConfig.cs
+++ src/Client/Assets/GraphicConfigs/Monsters/ChaserGraphicConfig.cs
@@ -3,19 +3,22 @@
 using Client.Core;
 using Client.Core.AnimationFrameSets;
 
+using Microsoft.Xna.Framework;
+
 namespace Client.Assets.GraphicConfigs.Monsters;
 
 internal sealed class ChaserGraphicConfig: EgyptianMonsterGraphicConfig
 {
     public ChaserGraphicConfig() : base(UnitName.Chaser)
     {
+        ShadowOrigin = new Vector2(70, 12);
     }
 
     public override IDictionary<PredefinedAnimationSid, IAnimationFrameSet> GetPredefinedAnimations()
     {
         return new Dictionary<PredefinedAnimationSid, IAnimationFrameSet>
         {
-            { PredefinedAnimationSid.Idle, new LinearAnimationFrameSet(new[]{0}, 0, frameWidth: 128, frameHeight: 156, textureColumns: 3) },
+            { PredefinedAnimationSid.Idle, new LinearAnimationFrameSet(new[]{0}, 0, frameWidth: 128, frameHeight: 150, textureColumns: 3) },
             {
                 PredefinedAnimationSid.MoveForward,
                 AnimationFrameSetFactory.CreateSequential(startFrameIndex: 0, frameCount: 1, fps: 1)
