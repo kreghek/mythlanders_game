0832a20c144e447cc93ad6cae5bdf507694d9bff
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-09-04 23:36:39 +0700

    WIP Conter-attack

---
+++ src/GameAssets.Combats/TargetSelectors/AttackerTargetSelector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CombatDicesTeam.Combats;
+
+namespace GameAssets.Combats.TargetSelectors;
+
+internal class AttackerTargetSelector : ITargetSelector
+{
+    public IReadOnlyList<ICombatant> GetMaterialized(ICombatant actor, ITargetSelectorContext context)
+    {
+        return new[] { context.Attacker! };
+    }
+}
