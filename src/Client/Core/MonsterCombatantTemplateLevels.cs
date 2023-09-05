d01fe93267fff415e47b6e52cf856c8e4749f243
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-09-05 14:07:47 +0700

    Clean up code

--- src/Client/Core/MonsterCombatantTemplateLevels.cs
+++ src/Client/Core/MonsterCombatantTemplateLevels.cs
@@ -3,6 +3,6 @@
 internal static class MonsterCombatantTemplateLevels
 {
     public static MonsterCombatantTempateLevel Easy => new(0);
-    public static MonsterCombatantTempateLevel Medium => new(1);
     public static MonsterCombatantTempateLevel Hard => new(2);
+    public static MonsterCombatantTempateLevel Medium => new(1);
 }
