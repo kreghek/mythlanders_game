59e1050d3981f0051c4ba74659dfd61017788106
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-09-25 15:13:55 +0700

    WIP Remake combat move execution sequence

--- src/GameAssets.Combats/CombatantStatuses/ConterAttackCombatantStatus.cs
+++ src/GameAssets.Combats/CombatantStatuses/ConterAttackCombatantStatus.cs
@@ -1,11 +1,30 @@
 using CombatDicesTeam.Combats;
 using CombatDicesTeam.Combats.CombatantStatuses;
 
+using GameAssets.Combats.TargetSelectors;
+
 namespace Core.Combats.CombatantStatuses;
 
 public sealed class ConterAttackCombatantStatus : CombatantStatusBase
 {
+    private readonly ITargetSelector _conterAttackTargetSelector;
+
     public ConterAttackCombatantStatus(ICombatantStatusSid sid, ICombatantStatusLifetime lifetime) : base(sid, lifetime)
     {
+        _conterAttackTargetSelector = new AttackerTargetSelector();
+    }
+
+    public override void Dispel(ICombatant combatant)
+    {
+    }
+
+    public override void Impose(ICombatant combatant, ICombatantStatusImposeContext context)
+    {
+        context.Combat.CombatantHasBeenDamaged += Combat_CombatantHasBeenDamaged;
+    }
+
+    private void Combat_CombatantHasBeenDamaged(object? sender, CombatantDamagedEventArgs e)
+    {
+        
     }
 }
