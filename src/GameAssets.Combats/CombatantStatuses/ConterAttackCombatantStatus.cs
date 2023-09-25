7cc871f3119eefa03951d44efb88b752617a609f
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-09-25 15:14:27 +0700

    WIP Remake combat move execution sequence - 2

--- src/GameAssets.Combats/CombatantStatuses/ConterAttackCombatantStatus.cs
+++ src/GameAssets.Combats/CombatantStatuses/ConterAttackCombatantStatus.cs
@@ -7,24 +7,42 @@ namespace Core.Combats.CombatantStatuses;
 
 public sealed class ConterAttackCombatantStatus : CombatantStatusBase
 {
-    private readonly ITargetSelector _conterAttackTargetSelector;
+
+    private readonly CombatMovement _combatMovement;
+    private CombatEngineBase? _combat;
+    private ICombatant? _ownerCombatant;
 
     public ConterAttackCombatantStatus(ICombatantStatusSid sid, ICombatantStatusLifetime lifetime) : base(sid, lifetime)
     {
-        _conterAttackTargetSelector = new AttackerTargetSelector();
+        
     }
 
     public override void Dispel(ICombatant combatant)
     {
+        _combat!.CombatantHasBeenDamaged -= Combat_CombatantHasBeenDamaged;
     }
 
     public override void Impose(ICombatant combatant, ICombatantStatusImposeContext context)
     {
-        context.Combat.CombatantHasBeenDamaged += Combat_CombatantHasBeenDamaged;
+        _combat = context.Combat;
+        _ownerCombatant = combatant;
+
+        _combat.CombatantHasBeenDamaged += Combat_CombatantHasBeenDamaged;
     }
 
     private void Combat_CombatantHasBeenDamaged(object? sender, CombatantDamagedEventArgs e)
     {
+        if (e.Combatant != _ownerCombatant)
+        {
+            return;
+        }
+
+        e.Combatant.Behaviour.HandleIntention()
         
+        var combat = (CombatEngineBase)sender!;
+
+
+        var execution = combat.CreateCombatMovementExecution(new CombatMovementInstance(_combatMovement));
+
     }
 }
