50c638aaf41af7c15d6a6b4c4caf3191379a9faf
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-12-25 15:18:10 +0700

    Add damage type

---
+++ src/Client/Assets/DamageType.cs
@@ -0,0 +1,32 @@
+﻿namespace Client.Assets;
+
+internal enum DamageType
+{
+    Rapid,
+    Concentrated,
+        
+    /// <summary>
+    /// Кинетическое.
+    /// </summary>
+    Kinetic,
+
+    /// <summary>
+    /// Взрывное.
+    /// </summary>
+    Explosion,
+
+    /// <summary>
+    /// Псионическое.
+    /// </summary>
+    Psy,
+
+    /// <summary>
+    /// Кислотное.
+    /// </summary>
+    Acid,
+
+    /// <summary>
+    /// Термальное.
+    /// </summary>
+    Termal
+}
