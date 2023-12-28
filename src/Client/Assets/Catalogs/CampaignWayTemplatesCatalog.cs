2ca852885c8c1477c7149cfadb29bcfe3fd56ba1
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-12-28 22:18:51 +0700

    Remove debug crisis

--- src/Client/Assets/Catalogs/CampaignWayTemplatesCatalog.cs
+++ src/Client/Assets/Catalogs/CampaignWayTemplatesCatalog.cs
@@ -48,8 +48,6 @@ internal sealed class CampaignWayTemplatesCatalog
 
         var way1Templates = new ICampaignStageTemplateFactory[]
         {
-            new CrisisEventCampaignStageTemplateFactory(_services),
-            
             // Combat
 
             new CombatCampaignStageTemplateFactory(locationSid, MonsterCombatantTemplateLevels.Easy, _services),
