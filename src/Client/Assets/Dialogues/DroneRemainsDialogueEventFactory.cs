410441580da2d2c53cde4d48c39dae4f46e9ee49
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-01-06 01:03:51 +0700

    Fix drone remains dialog sid

--- src/Client/Assets/Dialogues/DroneRemainsDialogueEventFactory.cs
+++ src/Client/Assets/Dialogues/DroneRemainsDialogueEventFactory.cs
@@ -7,6 +7,6 @@ namespace Client.Assets.Dialogues;
 [UsedImplicitly]
 internal sealed class DroneRemainsDialogueEventFactory : SmallEventDialogueEventFactoryBase
 {
-    protected override string DialogueFileSid => "dron_remains";
+    protected override string DialogueFileSid => "drone_remains";
     protected override string EventSid => CrisesCatalogSids.DroneRemains;
 }
