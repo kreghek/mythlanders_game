bcf3004f4abb23137479018731e76f3f42249d36
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-01-06 00:37:53 +0700

    Fix small events

--- src/Client/Assets/Dialogues/DroneRemainsDialogueEventFactory.cs
+++ src/Client/Assets/Dialogues/DroneRemainsDialogueEventFactory.cs
@@ -7,6 +7,6 @@ namespace Client.Assets.Dialogues;
 [UsedImplicitly]
 internal sealed class DroneRemainsDialogueEventFactory : SmallEventDialogueEventFactoryBase
 {
-    protected override string DialogueFileSid => CrisesCatalogSids.CrystalTreasures;
-    protected override string EventSid => "crystal_treasures";
+    protected override string DialogueFileSid => "dron_remains";
+    protected override string EventSid => CrisesCatalogSids.DroneRemains;
 }
