5c1b237fda000211ea1f72f528fc0713e6db8b90
kreghek <kreghek@users.noreply.github.com>
2024-01-10 15:51:19 +0000

    [Bot] Automated ReSharper CLT update

--- src/Client/GameScreens/Campaign/Ui/CampaignMap.cs
+++ src/Client/GameScreens/Campaign/Ui/CampaignMap.cs
@@ -550,7 +550,8 @@ internal sealed class CampaignMap : ControlBase
         if (campaignStageItem is CombatStageItem combat)
         {
             var combatSource = combat.CombatSequence.Combats.First();
-            var perkMonsterCombatantPrefab = combatSource.Monsters.OrderByDescending(x => x.StartUpStatuses.Count).First();
+            var perkMonsterCombatantPrefab =
+                combatSource.Monsters.OrderByDescending(x => x.StartUpStatuses.Count).First();
             var classSid = perkMonsterCombatantPrefab.TemplatePrefab.ClassSid;
             var monsterClass = Enum.Parse<UnitName>(classSid, true);
 
