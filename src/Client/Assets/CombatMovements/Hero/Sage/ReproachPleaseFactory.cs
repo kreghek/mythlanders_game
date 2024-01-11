56d99b0b1cb3ba30d655542c2fccd684dfeec851
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-01-12 01:42:06 +0700

    Add sage factory

---
+++ src/Client/Assets/CombatMovements/Hero/Sage/ReproachPleaseFactory.cs
@@ -0,0 +1,34 @@
+using CombatDicesTeam.Combats.Effects;
+using CombatDicesTeam.Combats;
+using CombatDicesTeam.GenericRanges;
+using Core.Combats.TargetSelectors;
+using GameAssets.Combats.CombatMovementEffects;
+
+namespace Client.Assets.CombatMovements.Hero.Sage;
+
+internal class ReproachPleaseFactory : CombatMovementFactoryBase
+{
+    /// <inheritdoc />
+    public override CombatMovementIcon CombatMovementIcon => new(3, 7);
+
+    /// <inheritdoc />
+    public override CombatMovement CreateMovement()
+    {
+        return new CombatMovement(Sid,
+            new CombatMovementCost(3),
+            CombatMovementEffectConfig.Create(
+                new IEffect[]
+                {
+                    new DamageEffectWrapper(
+                        new StrongestMarkedEnemyTargetSelector(),
+                        DamageType.Normal,
+                        GenericRange<int>.CreateMono(1)),
+                    new InterruptEffect(new SelfTargetSelector())
+                })
+        )
+        {
+            Tags = CombatMovementTags.Attack
+        };
+    }
+}
+
---
+++ src/Client/Core/Heroes/Factories/SageHeroFactory.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+using Client.Assets.CombatMovements.Hero.Sage;
+
+using CombatDicesTeam.Combats;
+
+using GameAssets.Combats;
+
+namespace Client.Core.Heroes.Factories;
+
+internal sealed class SageHeroFactory : HeroFactoryBase
+{
+    protected override string ClassSid => "sage";
+
+    protected override CombatMovementSequence CreateInitCombatMovementPool()
+    {
+        var movementPool = new List<CombatMovement>
+        {
+            CreateMovement<AskedNoViolenceFactory>(),
+
+            CreateMovement<FaithBoostFactory>(),
+
+            CreateMovement<NoViolencePleaseFactory>(),
+
+            CreateMovement<ReproachPleaseFactory>()
+        };
+
+        var heroSequence = new CombatMovementSequence();
+
+        for (var i = 0; i < 2; i++)
+        {
+            foreach (var movement in movementPool)
+            {
+                heroSequence.Items.Add(movement);
+            }
+        }
+
+        return heroSequence;
+    }
+
+    protected override CombatantStatsConfig CreateInitStats()
+    {
+        var stats = new CombatantStatsConfig();
+        stats.SetValue(CombatantStatTypes.HitPoints, 3);
+        stats.SetValue(CombatantStatTypes.ShieldPoints, 0);
+        stats.SetValue(CombatantStatTypes.Resolve, 3);
+        return stats;
+    }
+}
