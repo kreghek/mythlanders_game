4299aa5827bb324ba2d8537ef08662b71b92eecc
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-01-25 00:00:24 +0700

    WIP Insert values into skill description text

--- src/Client/GameScreens/Combat/Ui/CombatMovementHint.cs
+++ src/Client/GameScreens/Combat/Ui/CombatMovementHint.cs
@@ -16,22 +16,32 @@ internal class CombatMovementHint : HintBase
     private readonly CombatMovementInstance _combatMovement;
     private readonly IStatValue _currentActorResolveValue;
     private readonly string _combatMoveTitle;
-    private readonly SpriteFont _font;
-    private readonly SpriteFont _nameFont;
+    private readonly SpriteFont _descriptionTextFont;
+    private readonly SpriteFont _nameTextFont;
+    private readonly SpriteFont _costTextFont;
 
     public CombatMovementHint(CombatMovementInstance combatMovement, IStatValue currentActorResolveValue)
     {
-        _nameFont = UiThemeManager.UiContentStorage.GetTitlesFont();
-        _font = UiThemeManager.UiContentStorage.GetMainFont();
+        _nameTextFont = UiThemeManager.UiContentStorage.GetTitlesFont();
+        _descriptionTextFont = UiThemeManager.UiContentStorage.GetMainFont();
+        _costTextFont = UiThemeManager.UiContentStorage.GetMainFont();
         _combatMovement = combatMovement;
         _currentActorResolveValue = currentActorResolveValue;
+
         _combatMoveTitle = GameObjectHelper.GetLocalized(_combatMovement.SourceMovement.Sid);
 
+        var values = GetCombatMovementValues(combatMovement);
+
         _combatMoveDescription = StringHelper.LineBreaking(
-            GameObjectHelper.GetLocalizedDescription(_combatMovement.SourceMovement.Sid),
+            string.Format(GameObjectHelper.GetLocalizedDescription(_combatMovement.SourceMovement.Sid), values),
             60);
 
         ContentSize = CalcContentSize(_combatMoveTitle, _combatMoveDescription);
+    }
+
+    private object[] GetCombatMovementValues(CombatMovementInstance combatMovement)
+    {
+        throw new NotImplementedException();
     }
 
     public Vector2 ContentSize { get; }
@@ -47,7 +57,7 @@ internal class CombatMovementHint : HintBase
 
         var combatMoveTitlePosition = clientRect.Location.ToVector2();
 
-        spriteBatch.DrawString(_nameFont, _combatMoveTitle, combatMoveTitlePosition, color);
+        spriteBatch.DrawString(_nameTextFont, _combatMoveTitle, combatMoveTitlePosition, color);
 
         var resolveColor = color;
         if (_combatMovement.Cost.Amount.Current > _currentActorResolveValue.Current)
@@ -55,19 +65,19 @@ internal class CombatMovementHint : HintBase
             resolveColor = Color.Lerp(color, MythlandersColors.DangerRedMain, 0.75f);
         }
 
-        spriteBatch.DrawString(_font,
+        spriteBatch.DrawString(_descriptionTextFont,
             string.Format(UiResource.CombatMovementCostLabelTemplate, _combatMovement.Cost.Amount.Current, _currentActorResolveValue.Current),
             combatMoveTitlePosition + new Vector2(0, 15), resolveColor);
 
         var manaCostPosition = combatMoveTitlePosition + new Vector2(0, 25);
         var descriptionBlockPosition = manaCostPosition + new Vector2(0, 20);
-        spriteBatch.DrawString(_font, _combatMoveDescription, descriptionBlockPosition, Color.Wheat);
+        spriteBatch.DrawString(_descriptionTextFont, _combatMoveDescription, descriptionBlockPosition, Color.Wheat);
     }
 
     private Vector2 CalcContentSize(string combatMoveTitle, string combatMoveDescription)
     {
-        var titleSize = _nameFont.MeasureString(combatMoveTitle);
-        var descriptionSize = _font.MeasureString(combatMoveDescription);
+        var titleSize = _nameTextFont.MeasureString(combatMoveTitle);
+        var descriptionSize = _descriptionTextFont.MeasureString(combatMoveDescription);
 
         return new Vector2(
             Math.Max(titleSize.X, descriptionSize.X),
