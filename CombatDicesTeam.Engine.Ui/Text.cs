dfdb1705664b853cbbf52079191b75ea933d839a
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-01-29 11:43:40 +0700

    Add text and vertical stack ui elements

---
+++ CombatDicesTeam.Engine.Ui/Text.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Client.Engine;
+
+public sealed class Text : ControlBase
+{
+    private readonly Point _textureOffset;
+    private readonly SpriteFont _font;
+    private readonly Color _color;
+    private readonly Func<string> _textDelegate;
+
+    public Text(Texture2D texture, Point textureOffset, SpriteFont font, Color color, Func<string> textDelegate) : base(texture)
+    {
+        _textureOffset = textureOffset;
+        _font = font;
+        _color = color;
+        _textDelegate = textDelegate;
+    }
+
+    protected override Point CalcTextureOffset() => _textureOffset;
+
+    protected override Color CalculateColor() => _color;
+
+    protected override void DrawContent(SpriteBatch spriteBatch, Rectangle contentRect, Color contentColor)
+    {
+        spriteBatch.DrawString(_font, _textDelegate(), contentRect.Location.ToVector2(), contentColor);
+    }
+}
---
+++ CombatDicesTeam.Engine.Ui/VerticalStackPanel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Client.Engine;
+
+public sealed class VerticalStackPanel: ControlBase
+{
+    private readonly Point _textureOffset;
+    private readonly IReadOnlyList<ControlBase> _innerElements;
+
+    public VerticalStackPanel(Texture2D texture, Point textureOffset, IReadOnlyList<ControlBase> innerElements) : base(texture)
+    {
+        _textureOffset = textureOffset;
+        _innerElements = innerElements;
+    }
+
+    protected override Point CalcTextureOffset() => _textureOffset;
+
+    protected override Color CalculateColor() => Color.White;
+
+    protected override void DrawContent(SpriteBatch spriteBatch, Rectangle contentRect, Color contentColor)
+    {
+        var currentPosition = contentRect.Location;
+        for (var elementIndex = 0; elementIndex < _innerElements.Count; elementIndex++)
+        {
+            var innerElement = _innerElements[elementIndex];
+            innerElement.Rect = new Rectangle(currentPosition, innerElement.Rect.Size);
+            
+            innerElement.Draw(spriteBatch);
+
+            currentPosition += new Point(0, innerElement.Rect.Size.Y);
+        }
+    }
+}
