1d7c5a1200e1c75ac0d81025adf188391d03cf0c
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-19 22:48:35 +0700

    Test of Dialogue speaker

--- src/Client/Assets/Catalogs/Dialogues/DialogueSpeaker.cs
+++ src/Client/Assets/Catalogs/Dialogues/DialogueSpeaker.cs
@@ -7,14 +7,29 @@ using CombatDicesTeam.Dialogues;
 
 namespace Client.Assets.Catalogs.Dialogues;
 
-public sealed record DialogueSpeaker(UnitName Name) : IDialogueSpeaker, IEqualityComparer<DialogueSpeaker>
+public sealed record DialogueSpeaker(UnitName Name) : IDialogueSpeaker, IEqualityComparer<DialogueSpeaker>, IEqualityComparer<IDialogueSpeaker>
 {
     public bool Equals(DialogueSpeaker? x, DialogueSpeaker? y)
     {
         return x?.Name == y?.Name;
     }
 
+    public bool Equals(IDialogueSpeaker? x, IDialogueSpeaker? y)
+    {
+        if (x is DialogueSpeaker s1 && y is DialogueSpeaker s2)
+        { 
+            return Equals(s1, s2);
+        }
+
+        return false;
+    }
+
     public int GetHashCode([DisallowNull] DialogueSpeaker obj)
+    {
+        return Name.GetHashCode();
+    }
+
+    public int GetHashCode([DisallowNull] IDialogueSpeaker obj)
     {
         return Name.GetHashCode();
     }
