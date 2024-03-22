dabaee3762c0ff41d9a616e201684a8421eda3cd
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-22 13:26:08 +0700

    Move monster perks to factories

--- src/Client/Assets/MonsterPerks/IMonsterPerkFactory.cs
+++ src/Client/Assets/MonsterPerks/IMonsterPerkFactory.cs
@@ -1,45 +1,8 @@
 ﻿using Client.Core;
 
-using CombatDicesTeam.Combats;
-using CombatDicesTeam.Combats.CombatantEffectLifetimes;
-using CombatDicesTeam.Combats.CombatantStatuses;
-
-using GameAssets.Combats;
-using GameAssets.Combats.CombatantStatuses;
-
-using JetBrains.Annotations;
-
 namespace Client.Assets.MonsterPerks;
 
 public interface IMonsterPerkFactory
 {
     MonsterPerk Create();
-}
-
-public abstract class MonsterPerkFactoryBase : IMonsterPerkFactory
-{
-    protected string PerkName => GetType().Name[..^"MonsterPerkFactory".Length];
-
-    protected abstract ICombatantStatusFactory CreateStatus();
-
-
-    public MonsterPerk Create()
-    {
-        return new MonsterPerk(CreateStatus(), PerkName);
-    }
-}
-
-[UsedImplicitly]
-public sealed class ExtraHitPointsMonsterPerkFactory : MonsterPerkFactoryBase
-{
-    protected override ICombatantStatusFactory CreateStatus()
-    {
-        return new CombatStatusFactory(source =>
-            new AutoRestoreModifyStatCombatantStatus(new ModifyStatCombatantStatus(
-                new CombatantStatusSid(nameof(PerkName)),
-                new OwnerBoundCombatantEffectLifetime(),
-                source,
-                CombatantStatTypes.HitPoints,
-                1)));
-    }
 }
---
+++ src/Client/Assets/MonsterPerks/MonsterPerkFactoryBase.cs
@@ -0,0 +1,18 @@
+﻿using Client.Core;
+
+using CombatDicesTeam.Combats.CombatantStatuses;
+
+namespace Client.Assets.MonsterPerks;
+
+public abstract class MonsterPerkFactoryBase : IMonsterPerkFactory
+{
+    protected string PerkName => GetType().Name[..^"MonsterPerkFactory".Length];
+
+    protected abstract ICombatantStatusFactory CreateStatus();
+
+
+    public MonsterPerk Create()
+    {
+        return new MonsterPerk(CreateStatus(), PerkName);
+    }
+}
