0f962bef63d51110c2c3665ef097ded0636de86c
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-25 13:27:06 +0700

    Add monster perk - Reduce Shield Points of the heroes

---
+++ src/Client/Assets/MonsterPerks/ReduceEnemyShieldPointsMonsterPerkFactory.cs
@@ -0,0 +1,23 @@
+﻿using CombatDicesTeam.Combats;
+using CombatDicesTeam.Combats.CombatantEffectLifetimes;
+using CombatDicesTeam.Combats.CombatantStatuses;
+
+using GameAssets.Combats.CombatantStatuses;
+
+using JetBrains.Annotations;
+
+namespace Client.Assets.MonsterPerks;
+
+[UsedImplicitly]
+public sealed class ReduceEnemyShieldPointsMonsterPerkFactory : MonsterPerkFactoryBase
+{
+    protected override ICombatantStatusFactory CreateStatus()
+    {
+        return new CombatStatusFactory(source =>
+            new ReduceEnemiesShieldPointsCombatantStatus(
+                new CombatantStatusSid(nameof(PerkName)),
+                new OwnerBoundCombatantEffectLifetime(),
+                source,
+                1));
+    }
+}
