07ebefb713ea1794a07b2b97cfc6916eed3f38f7
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-26 13:51:48 +0700

    Use monster perk factories for pre-history perks

--- src/Client/Assets/MonsterPerks/MonsterPerkCatalog.cs
+++ src/Client/Assets/MonsterPerks/MonsterPerkCatalog.cs
@@ -14,32 +14,4 @@ public class MonsterPerkCatalog
     }
 
     public IReadOnlyCollection<MonsterPerk> Perks { get; }
-
-    public static MonsterPerk BlackMessah { get; } = new(new CombatStatusFactory(source =>
-            new ModifyEffectsCombatantStatus(new CombatantStatusSid(nameof(BlackMessah)),
-                new OwnerBoundCombatantEffectLifetime(),
-                source,
-                1)),
-        nameof(BlackMessah));
-
-    public static MonsterPerk UnitedRush { get; } = new(new CombatStatusFactory(source =>
-            new ModifyEffectsCombatantStatus(new CombatantStatusSid(nameof(UnitedRush)),
-                new OwnerBoundCombatantEffectLifetime(),
-                source,
-                1)),
-        nameof(UnitedRush));
-
-    public static MonsterPerk UnitedTactics { get; } = new(new CombatStatusFactory(source =>
-            new ModifyEffectsCombatantStatus(new CombatantStatusSid(nameof(UnitedTactics)),
-                new OwnerBoundCombatantEffectLifetime(),
-                source,
-                1)),
-        nameof(UnitedTactics));
-
-    public static MonsterPerk DefenderOfFaith { get; } = new(new CombatStatusFactory(source =>
-            new ModifyEffectsCombatantStatus(new CombatantStatusSid(nameof(DefenderOfFaith)),
-                new OwnerBoundCombatantEffectLifetime(),
-                source,
-                1)),
-        nameof(DefenderOfFaith));
 }
