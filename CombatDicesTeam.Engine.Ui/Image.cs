2637603d7fba03a78d2f0ac870731cbd5a01c4e0
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-03-27 16:46:06 +0700

    Add icon of monster perk

---
+++ CombatDicesTeam.Engine.Ui/Image.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CombatDicesTeam.Engine.Ui;
+
+public sealed class Image : ControlBase
+{
+    private readonly Texture2D _image;
+    private readonly Rectangle _sourceRect;
+    private readonly Point _textureOffset;
+
+    public Image(Texture2D image, Rectangle sourceRect, Texture2D texture, Point textureOffset) : base(texture)
+    {
+        _image = image;
+        _sourceRect = sourceRect;
+        _textureOffset = textureOffset;
+    }
+
+    protected override Point CalcTextureOffset() => _textureOffset;
+
+    protected override Color CalculateColor()
+    {
+        return Color.White;
+    }
+
+    protected override void DrawContent(SpriteBatch spriteBatch, Rectangle contentRect, Color contentColor)
+    {
+        spriteBatch.Draw(_image, contentRect.Location.ToVector2(), _sourceRect, contentColor);
+    }
+    
+    public override Point Size => _sourceRect.Size;
+}
--- src/Client/GameScreens/Bestiary/Ui/MonsterPerksPanel.cs
+++ src/Client/GameScreens/Bestiary/Ui/MonsterPerksPanel.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Client.Assets.CombatMovements;
 using Client.Core;
 using Client.Engine;
 
+using CombatDicesTeam.Combats;
 using CombatDicesTeam.Engine.Ui;
 
 using Microsoft.Xna.Framework;
@@ -22,27 +24,115 @@ public class MonsterPerksPanel: ControlBase
     {
         _monsterPerks = monsterPerks.OrderBy(x => x.Sid).ToArray();
 
-        var perkUiElements = CreatePerkUiElements(controlTextures: controlTextures, perkNameFont: perkNameFont);
+        var perkUiElements = CreatePerkUiElements(controlTextures, perkNameFont, perkDescriptionFont);
 
         _content = new VerticalStackPanel(controlTextures, ControlTextures.Transparent, perkUiElements);
     }
 
-    private List<ControlBase> CreatePerkUiElements(Texture2D controlTextures, SpriteFont perkNameFont)
+    private List<ControlBase> CreatePerkUiElements(Texture2D controlTextures, SpriteFont perkNameFont,
+        SpriteFont perkDescriptionFont)
     {
         var perkUiElements = new List<ControlBase>();
         foreach (var monsterPerk in _monsterPerks)
         {
-            var element = new Text(controlTextures,
+            
+            
+            var perkNameText = new Text(controlTextures,
                 ControlTextures.Transparent,
                 perkNameFont,
                 _ => Color.White,
                 () => GameObjectHelper.GetLocalized(monsterPerk.Sid)
             );
+            
+            var perkDescriptionText = new RichText(controlTextures,
+                ControlTextures.Transparent,
+                perkDescriptionFont,
+                _ => MythlandersColors.Description,
+                () => CalcMonsterPerkDescription(monsterPerk)
+            );
 
-            perkUiElements.Add(element);
+            var perkRightElement = new VerticalStackPanel(controlTextures, ControlTextures.Transparent, new ControlBase[]
+            {
+                perkNameText,
+                perkDescriptionText
+            });
+
+            var iconLeftImage = new Image(controlTextures, new Rectangle(0, 0, 64, 64), controlTextures,
+                ControlTextures.Transparent);
+
+            var perkElement = new HorizontalStackPanel(controlTextures, ControlTextures.Transparent,
+                new ControlBase[]
+                {
+                    iconLeftImage, perkRightElement
+                });
+
+            perkUiElements.Add(perkElement);
         }
 
         return perkUiElements;
+    }
+    
+    private static string CalcMonsterPerkDescription(MonsterPerk monsterPerk)
+    {
+        var perkDisplayValues = ExtractCombatMovementValues(monsterPerk);
+
+        var perkSid = monsterPerk.Sid;
+        var perkDescription =
+            RenderDescriptionText(perkDisplayValues, perkSid);
+        return perkDescription;
+    }
+
+    private static IReadOnlyList<CombatMovementEffectDisplayValue> ExtractCombatMovementValues(MonsterPerk monsterPerk)
+    {
+        return ArraySegment<CombatMovementEffectDisplayValue>.Empty;
+    }
+
+    private static string RenderDescriptionText(IReadOnlyList<CombatMovementEffectDisplayValue> values,
+        CombatMovementSid combatMovementSid)
+    {
+        var descriptionMarkupText =
+            StringHelper.LineBreaking(GameObjectHelper.GetLocalizedDescription(combatMovementSid), 60);
+
+        foreach (var value in values)
+        {
+            var valueText = GetValueText(value);
+            var styledValueText = $"<style=color1>{valueText}</style>";
+            descriptionMarkupText = descriptionMarkupText.Replace($"<{value.Tag}>", styledValueText);
+        }
+
+        return descriptionMarkupText;
+    }
+    
+    private static string GetValueText(CombatMovementEffectDisplayValue value)
+    {
+        var template = GetValueTemplate(value.Template);
+        return string.Format(template, value.Value);
+    }
+    
+    private static string GetValueTemplate(CombatMovementEffectDisplayValueTemplate valueType)
+    {
+        return valueType switch
+        {
+            CombatMovementEffectDisplayValueTemplate.Damage =>
+                UiResource.CombatMovementEffectValueType_Damage_Template,
+            CombatMovementEffectDisplayValueTemplate.DamageModifier =>
+                UiResource.CombatMovementEffectValueType_DamageModifer_Template,
+            CombatMovementEffectDisplayValueTemplate.TurnDuration =>
+                UiResource.CombatMovementEffectValueType_TurnDuration_Template,
+            CombatMovementEffectDisplayValueTemplate.RoundDuration =>
+                UiResource.CombatMovementEffectValueType_RoundDuration_Template,
+            CombatMovementEffectDisplayValueTemplate.HitPointsDamage =>
+                UiResource.CombatMovementEffectValueType_HitPointsDamage_Template,
+            CombatMovementEffectDisplayValueTemplate.ResolveDamage =>
+                UiResource.CombatMovementEffectValueType_ResolveDamage_Template,
+            CombatMovementEffectDisplayValueTemplate.HitPoints =>
+                UiResource.CombatMovementEffectValueType_HitPoints_Template,
+            CombatMovementEffectDisplayValueTemplate.ShieldPoints =>
+                UiResource.CombatMovementEffectValueType_ShieldPoints_Template,
+            CombatMovementEffectDisplayValueTemplate.Defence =>
+                UiResource.CombatMovementEffectValueType_Defence_Template,
+            _ => "<{0}> units"
+        };
     }
 
     protected override Point CalcTextureOffset() => ControlTextures.Transparent;
