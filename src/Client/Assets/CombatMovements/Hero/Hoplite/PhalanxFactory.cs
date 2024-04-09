07eda5fb9cec42e7cde44bd4181bd0c5a8f1df95
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-04-09 14:22:21 +0700

    WIP Restore phalanx

--- src/Client/Assets/CombatMovements/Hero/Hoplite/PhalanxFactory.cs
+++ src/Client/Assets/CombatMovements/Hero/Hoplite/PhalanxFactory.cs
@@ -1,6 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
+
+using Client.Core;
 
 using CombatDicesTeam.Combats;
+using CombatDicesTeam.Combats.CombatantEffectLifetimes;
+using CombatDicesTeam.Combats.CombatantStatuses;
 using CombatDicesTeam.Combats.Effects;
 using CombatDicesTeam.GenericRanges;
 
@@ -8,6 +13,7 @@ using Core.Combats.Effects;
 using Core.Combats.TargetSelectors;
 
 using GameAssets.Combats;
+using GameAssets.Combats.CombatantStatuses;
 using GameAssets.Combats.CombatMovementEffects;
 
 using JetBrains.Annotations;
@@ -35,23 +41,113 @@ internal class PhalanxFactory : SimpleCombatMovementFactoryBase
     /// <inheritdoc />
     protected override CombatMovementEffectConfig GetEffects()
     {
-        return CombatMovementEffectConfig.Create(new IEffect[]
-        {
-            new DamageEffectWrapper(
-                new ClosestInLineTargetSelector(),
-                DamageType.Normal,
-                GenericRange<int>.CreateMono(2)),
-            new PushToPositionEffect(
-                new SelfTargetSelector(),
-                ChangePositionEffectDirection.ToVanguard
-            )
-        });
+        return new CombatMovementEffectConfig(new IEffect[]
+            {
+                new AddCombatantStatusEffect(new SelfTargetSelector(),
+                    new CombatStatusFactory(source =>
+                        new DefensiveStanceCombatantStatusWrapper(
+                            new AutoRestoreModifyStatCombatantStatus(
+                                new ModifyStatCombatantStatus(
+                                    new CombatantStatusSid(Sid),
+                                    new ToNextCombatantTurnEffectLifetime(),
+                                    source,
+                                    CombatantStatTypes.Defense,
+                                    1)))
+                    )
+                ),
+                new AddCombatantStatusEffect(new LeftAllyTargetSelector(),
+                    new CombatStatusFactory(source =>
+                        new DefensiveStanceCombatantStatusWrapper(
+                            new AutoRestoreModifyStatCombatantStatus(
+                                new ModifyStatCombatantStatus(
+                                    new CombatantStatusSid(Sid),
+                                    new ToNextCombatantTurnEffectLifetime(),
+                                    source,
+                                    CombatantStatTypes.Defense,
+                                    2))
+                        )
+                    )
+                )
+                {
+                    ImposeConditions = { new IsRightAllyWithShieldCondition() }
+                }
+            },
+            new IEffect[]
+            {
+                new AddCombatantStatusEffect(new SelfTargetSelector(),
+                    new CombatStatusFactory(source =>
+                        new DefensiveStanceCombatantStatusWrapper(
+                            new AutoRestoreModifyStatCombatantStatus(
+                                new ModifyStatCombatantStatus(
+                                    new CombatantStatusSid(Sid),
+                                    new ToEndOfCurrentRoundEffectLifetime(),
+                                    source,
+                                    CombatantStatTypes.Defense,
+                                    1))
+                        )
+                    )
+                )
+            });
     }
 
     /// <inheritdoc />
     protected override CombatMovementTags GetTags()
     {
         return CombatMovementTags.Attack;
+    }
+
+    private sealed class IsRightAllyWithShieldCondition : IEffectCondition
+    {
+        public bool Check()
+        {
+            ICombatant effectActor = null;
+            CombatField field = null;
+
+            var rightAllyCombatant = GetRightAlly(effectActor, field);
+
+            if (rightAllyCombatant is null)
+            {
+                return false;
+            }
+
+            var isRightCombatantHasShield = CheckIsCombatantHasShield(rightAllyCombatant);
+
+            return isRightCombatantHasShield;
+        }
+
+        private static bool CheckIsCombatantHasShield(ICombatant testCombatant)
+        {
+            return
+                testCombatant.Statuses
+                    .Any(x=>ReferenceEquals(x, SystemStatuses.HasShield));
+            //.ClassSid is "Swordsman" or "Hoplite" or "Guardian" or "Assaulter" or "Liberator";
+        }
+    }
+
+    private static ICombatant? GetRightAlly(ICombatant baseCombatant, CombatField field)
+    {
+        var side = GetTargetSide(baseCombatant, field);
+        var currentCoords = side.GetCombatantCoords(baseCombatant);
+
+        if (currentCoords.LineIndex == 2)
+        {
+            return null;
+        }
+
+        var rightCoords = new FieldCoords(currentCoords.ColumentIndex, currentCoords.LineIndex + 2);
+
+        return side[rightCoords].Combatant;
+    }
+    
+    private static CombatFieldSide GetTargetSide(ICombatant target, CombatField field)
+    {
+        var heroes = field.HeroSide.GetAllCombatants();
+        if (heroes.Contains(target))
+        {
+            return field.HeroSide;
+        }
+
+        return field.MonsterSide;
     }
 }
 
---
+++ src/GameAssets.Combats/CombatantStatuses/HasShieldStatus.cs
@@ -0,0 +1,48 @@
+﻿using CombatDicesTeam.Combats;
+using CombatDicesTeam.Combats.CombatantEffectLifetimes;
+
+namespace GameAssets.Combats.CombatantStatuses;
+
+public sealed class NullCombatantStatusSource : ICombatantStatusSource
+{
+    
+}
+
+public abstract class SystemCombatantStatus:ICombatantStatus
+{
+    public abstract void Dispel(ICombatant combatant);
+    public abstract void Impose(ICombatant combatant, ICombatantStatusImposeContext combatantEffectImposeContext);
+    public abstract void Update(CombatantStatusUpdateType updateType, ICombatantStatusLifetimeUpdateContext context);
+    public abstract ICombatantStatusLifetime Lifetime { get; }
+    public abstract ICombatantStatusSid Sid { get; }
+    public abstract ICombatantStatusSource Source { get; }
+}
+
+public sealed class HasShieldCombatantStatus: SystemCombatantStatus
+{
+    
+    
+    public override void Dispel(ICombatant combatant)
+    {
+        
+    }
+
+    public override void Impose(ICombatant combatant, ICombatantStatusImposeContext combatantEffectImposeContext)
+    {
+        
+    }
+
+    public override void Update(CombatantStatusUpdateType updateType, ICombatantStatusLifetimeUpdateContext context)
+    {
+        
+    }
+
+    public override ICombatantStatusLifetime Lifetime { get; } = new OwnerBoundCombatantEffectLifetime();
+    public override ICombatantStatusSid Sid { get; } = new CombatantStatusSid("HasShield");
+    public override ICombatantStatusSource Source { get; } = Singleton<NullCombatantStatusSource>.Instance;
+}
+
+public static class SystemStatuses
+{
+    public static ICombatantStatus HasShield { get; } = new HasShieldCombatantStatus();
+}
