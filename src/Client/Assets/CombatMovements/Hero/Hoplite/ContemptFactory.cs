7e4893ff6feba758a9404fd1c08311f8adbd45cb
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-04-12 02:44:26 +0700

    WIP Two-phase contempt skill visualization

--- src/Client/Assets/CombatMovements/Hero/Hoplite/ContemptFactory.cs
+++ src/Client/Assets/CombatMovements/Hero/Hoplite/ContemptFactory.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
 
+using Client.Assets.CombatVisualEffects;
+using Client.Engine;
+using Client.GameScreens;
+
 using CombatDicesTeam.Combats;
 using CombatDicesTeam.Combats.Effects;
 using CombatDicesTeam.GenericRanges;
@@ -9,8 +13,13 @@ using Core.Combats.TargetSelectors;
 
 using GameAssets.Combats;
 using GameAssets.Combats.CombatMovementEffects;
+using GameClient.Engine.Animations;
+
+using GameClient.Engine.CombatVisualEffects;
 
 using JetBrains.Annotations;
+
+using MonoGame.Extended.TextureAtlases;
 
 namespace Client.Assets.CombatMovements.Hero.Hoplite;
 
@@ -46,7 +55,7 @@ internal class ContemptFactory : SimpleCombatMovementFactoryBase
                 DamageType.Normal,
                 GenericRange<int>.CreateMono(1)),
             new PushToPositionEffect(
-                new SelfTargetSelector(),
+                new ClosestInLineTargetSelector(),
                 ChangePositionEffectDirection.ToVanguard
             )
         });
@@ -56,5 +65,77 @@ internal class ContemptFactory : SimpleCombatMovementFactoryBase
     protected override CombatMovementTags GetTags()
     {
         return CombatMovementTags.Attack;
+    }
+
+    public override CombatMovementScene CreateVisualization(IActorAnimator actorAnimator,
+        CombatMovementExecution movementExecution, ICombatMovementVisualizationContext visualizationContext)
+    {
+        var swordsmanAnimationSet = visualizationContext.GameObjectContentStorage.GetAnimation("Hoplite");
+
+        var keepSwordStrongerAnimation = AnimationHelper.ConvertToAnimation(swordsmanAnimationSet, "prepare-spear");
+        var keepSwordSoundEffect =
+            visualizationContext.GameObjectContentStorage.GetSkillUsageSound(GameObjectSoundType.SwordPrepare);
+
+        var chargeAnimation = AnimationHelper.ConvertToAnimation(swordsmanAnimationSet, "charge");
+        var chargeSoundEffect =
+            visualizationContext.GameObjectContentStorage.GetSkillUsageSound(GameObjectSoundType.ArmedMove);
+
+        var hitAnimation = AnimationHelper.ConvertToAnimation(swordsmanAnimationSet, "spear-hit");
+        var swordHitSoundEffect =
+            visualizationContext.GameObjectContentStorage.GetSkillUsageSound(GameObjectSoundType.SwordSlash);
+
+        var hitCompleteAnimation = AnimationHelper.ConvertToAnimation(swordsmanAnimationSet, "hit-complete");
+
+        var backAnimation = AnimationHelper.ConvertToAnimation(swordsmanAnimationSet, "back");
+
+        var config = new SingleMeleeVisualizationConfig(
+            new SoundedAnimation(keepSwordStrongerAnimation, keepSwordSoundEffect.CreateInstance()),
+            new SoundedAnimation(chargeAnimation, chargeSoundEffect.CreateInstance()),
+            new SoundedAnimation(hitAnimation, swordHitSoundEffect.CreateInstance()),
+            new SoundedAnimation(hitCompleteAnimation, null),
+            new SoundedAnimation(backAnimation, null));
+
+        return CommonCombatVisualization.CreateSingleMeleeVisualization(actorAnimator, movementExecution,
+            visualizationContext, config);
+
+        var animationSet = visualizationContext.GameObjectContentStorage.GetAnimation("Partisan");
+
+        var prepareAnimation = AnimationHelper.ConvertToAnimation(animationSet, "prepare-rifle");
+
+        var shotSoundEffect =
+            visualizationContext.GameObjectContentStorage.GetSkillUsageSound(GameObjectSoundType.CyberRifleShot);
+        var shotAnimation = AnimationHelper.ConvertToAnimation(animationSet, "rifle-shot");
+        var soundedShotAnimation = new SoundedAnimationFrameSet(shotAnimation,
+            new[]
+            {
+                new AnimationFrame<IAnimationSoundEffect>(new AnimationFrameInfo(1),
+                    new AnimationSoundEffect(shotSoundEffect, new AudioSettings()))
+            });
+
+        var targetPosition =
+            AnimationHelper.GetTargetPositionByCombatMovementCombatant(movementExecution, visualizationContext);
+
+        var shotEffect = new ParallelCombatVisualEffect(
+            new PowderGasesCombatVisualEffect(visualizationContext.ActorGameObject.LaunchPoint, targetPosition,
+                new TextureRegion2D(visualizationContext.GameObjectContentStorage.GetParticlesTexture(),
+                    new Rectangle(0, 32 * 1, 32, 32))),
+            new GunFlashCombatVisualEffect(visualizationContext.ActorGameObject.LaunchPoint, 48,
+                new TextureRegion2D(visualizationContext.GameObjectContentStorage.GetParticlesTexture(),
+                    new Rectangle(0, 32 * 1, 32, 32))));
+
+        var additionalVisualEffectShotAnimation = new CombatVisualEffectAnimationFrameSet(soundedShotAnimation,
+            visualizationContext.CombatVisualEffectManager,
+            new[]
+            {
+                new AnimationFrame<ICombatVisualEffect>(new AnimationFrameInfo(1), shotEffect)
+            });
+
+        var waitAnimation = AnimationHelper.ConvertToAnimation(animationSet, "rifle-wait");
+
+        var projectileFactory = new InteractionDeliveryFactory(GetCreateProjectileFunc(visualizationContext));
+        return CommonCombatVisualization.CreateSingleDistanceVisualization(actorAnimator, movementExecution,
+            visualizationContext,
+            new SingleDistanceVisualizationConfig(prepareAnimation, additionalVisualEffectShotAnimation, waitAnimation,
+                projectileFactory, new AnimationFrameInfo(1)));
     }
 }
