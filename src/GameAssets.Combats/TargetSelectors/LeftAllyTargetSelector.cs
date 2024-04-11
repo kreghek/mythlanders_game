ba9df723267697890885163a7778c448a05e1921
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-04-11 22:29:50 +0700

    Fix LeftAllyTargetSelector

--- src/GameAssets.Combats/TargetSelectors/LeftAllyTargetSelector.cs
+++ src/GameAssets.Combats/TargetSelectors/LeftAllyTargetSelector.cs
@@ -6,6 +6,26 @@ public sealed class LeftAllyTargetSelector : ITargetSelector
 {
     public IReadOnlyList<ICombatant> GetMaterialized(ICombatant actor, ITargetSelectorContext context)
     {
-        throw new NotImplementedException();
+        var target = GetLeftAlly(actor, context.ActorSide);
+        if (target is null)
+        { 
+            return Array.Empty<ICombatant>();
+        }
+
+        return new[] { target };
+    }
+
+    private static ICombatant? GetLeftAlly(ICombatant baseCombatant, CombatFieldSide side)
+    {
+        var currentCoords = side.GetCombatantCoords(baseCombatant);
+
+        if (currentCoords.LineIndex == 0)
+        {
+            return null;
+        }
+
+        var rightCoords = new FieldCoords(currentCoords.ColumentIndex, currentCoords.LineIndex - 1);
+
+        return side[rightCoords].Combatant;
     }
 }
