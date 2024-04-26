82891467fb02e013f53645adb150d73c33786645
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-04-26 13:06:33 +0700

    Features to enable mini games

---
+++ src/Client/Assets/Catalogs/CampaignGeneration/MiniGameEventCampaignStageTemplateFactory.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Client.Assets.StageItems;
+using Client.Core;
+using Client.Core.Campaigns;
+
+using CombatDicesTeam.Dices;
+using CombatDicesTeam.Graphs;
+using CombatDicesTeam.Graphs.Generation.TemplateBased;
+
+namespace Client.Assets.Catalogs.CampaignGeneration;
+
+internal sealed class MiniGameEventCampaignStageTemplateFactory : ICampaignStageTemplateFactory
+{
+    private readonly CampaignStageTemplateServices _services;
+
+    public MiniGameEventCampaignStageTemplateFactory(CampaignStageTemplateServices services)
+    {
+        _services = services;
+    }
+
+    private static ICampaignStageItem[] MapContextToCurrentStageItems(IGraphTemplateContext<ICampaignStageItem> context)
+    {
+        return context.CurrentWay.Select(x => x.Payload).ToArray();
+    }
+
+    public bool CanCreate(IReadOnlyList<ICampaignStageItem> currentStageItems)
+    {
+        return _allMiniGameFeatures.Any(x => _services.GlobeProvider.Globe.Features.HasFeature(x));
+    }
+
+    public ICampaignStageItem Create(IReadOnlyList<ICampaignStageItem> currentStageItems)
+    {
+        var rolledMiniGameFeature = RollMiniGameFeature();
+
+        if (rolledMiniGameFeature == GameFeatures.SlidingPuzzleMiniGame)
+        {
+            return new SlidingPuzzlesMiniGameStageItem();
+        }
+
+        if (rolledMiniGameFeature == GameFeatures.TowersMiniGame)
+        {
+            return new TowersMinigameStageItem();
+        }
+
+        if (rolledMiniGameFeature == GameFeatures.Match3MiniGame)
+        {
+            return new Match3MinigameStageItem();
+        }
+
+        // rollback
+        return new SlidingPuzzlesMiniGameStageItem();
+    }
+
+    private readonly IReadOnlyCollection<GameFeature> _allMiniGameFeatures = new[]
+    {
+        GameFeatures.SlidingPuzzleMiniGame, GameFeatures.Match3MiniGame, GameFeatures.TowersMiniGame
+    };
+
+    private GameFeature RollMiniGameFeature()
+    {
+        var availableMiniGames = FilterMiniGameFeatures(_allMiniGameFeatures);
+
+        var rolledMiniGameFeatures = _services.Dice.RollFromList(availableMiniGames.ToArray());
+
+        return rolledMiniGameFeatures;
+    }
+
+    private IEnumerable<GameFeature> FilterMiniGameFeatures(IEnumerable<GameFeature> allMiniGameFeatures)
+    {
+        return allMiniGameFeatures.Where(x => _services.GlobeProvider.Globe.Features.HasFeature(x)).ToArray();
+    }
+
+    /// <inheritdoc />
+    public IGraphNode<ICampaignStageItem> Create(IGraphTemplateContext<ICampaignStageItem> context)
+    {
+        return new GraphNode<ICampaignStageItem>(Create(MapContextToCurrentStageItems(context)));
+    }
+
+    /// <inheritdoc />
+    public bool CanCreate(IGraphTemplateContext<ICampaignStageItem> context)
+    {
+        return CanCreate(MapContextToCurrentStageItems(context));
+    }
+}
--- src/Client/Core/GameFeatures.cs
+++ src/Client/Core/GameFeatures.cs
@@ -2,7 +2,10 @@
 
 public static class GameFeatures
 {
-    public static GameFeature CampaignMap { get; } = new GameFeature(nameof(CampaignMap));
-    public static GameFeature Campaigns { get; } = new GameFeature(nameof(Campaigns));
-    public static GameFeature SideQuests { get; } = new GameFeature(nameof(SideQuests));
+    public static GameFeature CampaignMap { get; } = new(nameof(CampaignMap));
+    public static GameFeature Campaigns { get; } = new(nameof(Campaigns));
+    public static GameFeature SideQuests { get; } = new(nameof(SideQuests));
+    public static GameFeature SlidingPuzzleMiniGame{ get; } = new(nameof(SlidingPuzzleMiniGame));
+    public static GameFeature Match3MiniGame{ get; } = new(nameof(Match3MiniGame));
+    public static GameFeature TowersMiniGame{ get; } = new(nameof(TowersMiniGame));
 }
