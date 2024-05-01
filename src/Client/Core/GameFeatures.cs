943ab46eea722288f56fe85ab055c3da0ca19e4d
kreghek <kreghek@users.noreply.github.com>
2024-05-01 04:00:56 +0000

    [Bot] Automated ReSharper CLT update

--- src/Client/Core/GameFeatures.cs
+++ src/Client/Core/GameFeatures.cs
@@ -4,7 +4,7 @@ public static class GameFeatures
 {
     public static GameFeature CampaignMap { get; } = new(nameof(CampaignMap));
     public static GameFeature Campaigns { get; } = new(nameof(Campaigns));
+    public static GameFeature RewardMonsterPerks { get; } = new(nameof(RewardMonsterPerks));
     public static GameFeature SideQuests { get; } = new(nameof(SideQuests));
     public static GameFeature UseMonsterPerks { get; } = new(nameof(UseMonsterPerks));
-    public static GameFeature RewardMonsterPerks { get; } = new(nameof(RewardMonsterPerks));
 }
