a82d6334130c8892fb8dcef5e4281b28af4fc199
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-05-18 13:42:06 +0700

    WIP Save game settings

--- src/Client/GameSettings.cs
+++ src/Client/GameSettings.cs
@@ -1,10 +1,26 @@
-﻿namespace Client;
+﻿using System.IO;
+using System;
+
+namespace Client;
 
 internal sealed class GameSettings
 {
+    private string _storagePath;
+
+    public GameSettings()
+    {
+        var binPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        _storagePath = Path.Combine(binPath, "CDT", "Mythlanders");
+    }
+
     public bool IsRecordMode { get; set; } = true;
     public GameMode Mode { get; init; }
     public float MusicVolume { get; set; } = 1.0f;
 
     public string ShieldSound { get; set; } = "Shield";
+
+    public void Save()
+    {
+        
+    }
 }
