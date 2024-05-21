a9bfd56caae62b7108c8c2719005a5a36abb280f
Pavel Kurkutov <kurkutovpa@gmail.com>
2024-05-21 12:28:30 +0700

    Aspid's Dark Raids reduces damage of ranged attacks

--- src/Client/Assets/CombatMovements/Monster/Slavic/Aspid/DarkRaidsFactory.cs
+++ src/Client/Assets/CombatMovements/Monster/Slavic/Aspid/DarkRaidsFactory.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 
 using CombatDicesTeam.Combats;
+using CombatDicesTeam.Combats.CombatantEffectLifetimes;
+using CombatDicesTeam.Combats.CombatantStatuses;
 using CombatDicesTeam.Combats.Effects;
 using CombatDicesTeam.GenericRanges;
 
@@ -8,7 +10,11 @@ using Core.Combats.Effects;
 using Core.Combats.TargetSelectors;
 
 using GameAssets.Combats;
+using GameAssets.Combats.AuraTargetSelectors;
+using GameAssets.Combats.CombatantStatuses;
 using GameAssets.Combats.CombatMovementEffects;
+
+using SelfTargetSelector = Core.Combats.TargetSelectors.SelfTargetSelector;
 
 namespace Client.Assets.CombatMovements.Monster.Slavic.Aspid;
 
@@ -20,8 +26,10 @@ internal class DarkRaidsFactory : SimpleCombatMovementFactoryBase
     {
         return new[]
         {
-            new DescriptionKeyValue("damage", ExtractDamage(combatMovementInstance, 1),
-                DescriptionKeyValueTemplate.Damage)
+            new DescriptionKeyValue("damage", ExtractDamage(combatMovementInstance, 0),
+                DescriptionKeyValueTemplate.Damage),
+            new DescriptionKeyValue("duration", 3, DescriptionKeyValueTemplate.TurnDuration),
+            new DescriptionKeyValue("ranged_attack_debuff", 2, DescriptionKeyValueTemplate.DamageModifier),
         };
     }
 
@@ -43,18 +51,23 @@ internal class DarkRaidsFactory : SimpleCombatMovementFactoryBase
         return CombatMovementEffectConfig.Create(
             new IEffect[]
             {
-                new AdjustPositionEffect(new SelfTargetSelector()),
                 new DamageEffectWrapper(
-                    new ClosestInLineTargetSelector(),
+                    new WeakestEnemyTargetSelector(),
                     DamageType.Normal,
                     GenericRange<int>.CreateMono(2)),
-                new DamageEffectWrapper(
-                    new ClosestInLineTargetSelector(),
-                    DamageType.Normal,
-                    GenericRange<int>.CreateMono(2)),
-                new PushToPositionEffect(
-                    new SelfTargetSelector(),
-                    ChangePositionEffectDirection.ToVanguard)
+                new AddCombatantStatusEffect(
+                    new SelfTargetSelector(), 
+                    new CombatStatusFactory(source => new AuraCombatantStatus(
+                        new CombatantStatusSid(Sid),
+                        new MultipleCombatantTurnEffectLifetime(3), 
+                        source,
+                        combatant => new CombatStatusFactory(source2 => 
+                            new ModifyDamageCalculatedCombatantStatus(
+                                new CombatantStatusSid(Sid),
+                                new OwnerBoundCombatantEffectLifetime(),
+                                source2,
+                                combatant1 => 2)), 
+                        new EnemyRearguardAuraTargetSelector())))
             });
     }
 
